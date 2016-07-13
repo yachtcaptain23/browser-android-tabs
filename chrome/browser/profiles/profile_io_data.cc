c3b91f2060040e7ee1329826a527e6f9caf103f9
Serg <serg.zhukovsky@gmail.com>
2016-07-13 10:35:42 -0400

    AdBlock, tracking protection, h264, aac support
    Auditors: @bbondy
    
    Conflicts:
    	chrome/android/BUILD.gn
    	out/Default/obj/chrome/android/chrome_public_apk_assets__build_config.ninja
    	out/Default/obj/chrome/android/chrome_public_apk_manifest.ninja
    	out/Default/obj/chrome/android/chrome_public_apk_resources__build_config.ninja
    	out/Default/obj/chrome/android/chrome_public_apk_resources__process_resources.ninja
    	out/Default/obj/chrome/android/chrome_public_apk_template_resources__template.ninja
    	out/Default/obj/chrome/android/chrome_public_test_apk_manifest.ninja
    	out/Default/obj/chrome/browser/browser.ninja
    
    Conflicts:
    	chrome/android/chrome_apk.gyp
    	chrome/browser/net/chrome_network_delegate.cc
    	chrome/chrome_android_paks.gypi
    	chrome/chrome_browser.gypi
    	media/base/key_systems.cc
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
    	chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
    	chrome/android/java/strings/android_chrome_strings.grd
    	chrome/common/pref_names.cc
    	chrome/common/pref_names.h

--- chrome/browser/profiles/profile_io_data.cc
+++ chrome/browser/profiles/profile_io_data.cc
@@ -455,6 +455,8 @@ void ProfileIOData::InitializeOnUIThread(Profile* profile) {
   ChromeNetworkDelegate::InitializePrefsOnUIThread(
       &enable_referrers_,
       &enable_do_not_track_,
+      &enable_tracking_protection_,
+      &enable_ad_block_,
       &force_google_safesearch_,
       &force_youtube_restrict_,
       &allowed_domains_for_apps_,
@@ -1038,6 +1040,8 @@ void ProfileIOData::Init(
   chrome_network_delegate->set_cookie_settings(
       profile_params_->cookie_settings.get());
   chrome_network_delegate->set_enable_do_not_track(&enable_do_not_track_);
+  chrome_network_delegate->set_enable_tracking_protection(&enable_tracking_protection_);
+  chrome_network_delegate->set_enable_ad_block(&enable_ad_block_);
   chrome_network_delegate->set_force_google_safe_search(
       &force_google_safesearch_);
   chrome_network_delegate->set_force_youtube_restrict(&force_youtube_restrict_);
@@ -1295,6 +1299,8 @@ void ProfileIOData::ShutdownOnUIThread(
   sync_first_setup_complete_.Destroy();
   enable_referrers_.Destroy();
   enable_do_not_track_.Destroy();
+  enable_tracking_protection_.Destroy();
+  enable_ad_block_.Destroy();
   force_google_safesearch_.Destroy();
   force_youtube_restrict_.Destroy();
   allowed_domains_for_apps_.Destroy();
