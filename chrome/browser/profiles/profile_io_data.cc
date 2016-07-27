c7791aab941d1861270efe55771bae696e8749bb
Serg <serg.zhukovsky@gmail.com>
2016-07-27 17:27:24 -0400

    Added HTTPSE support
    Auditors: @diracdeltas
    
    Conflicts:
    	chrome/browser/net/chrome_network_delegate.cc
    
    Conflicts:
    	chrome/browser/net/chrome_network_delegate.cc
    
    Conflicts:
    	chrome/android/java/strings/android_chrome_strings.grd

--- chrome/browser/profiles/profile_io_data.cc
+++ chrome/browser/profiles/profile_io_data.cc
@@ -505,6 +505,7 @@ void ProfileIOData::InitializeOnUIThread(Profile* profile) {
   ChromeNetworkDelegate::InitializePrefsOnUIThread(
       &enable_referrers_,
       &enable_do_not_track_,
+      &enable_httpse_,
       &enable_tracking_protection_,
       &enable_ad_block_,
       &force_google_safesearch_,
@@ -1088,6 +1089,7 @@ void ProfileIOData::Init(
   chrome_network_delegate->set_cookie_settings(
       profile_params_->cookie_settings.get());
   chrome_network_delegate->set_enable_do_not_track(&enable_do_not_track_);
+  chrome_network_delegate->set_enable_httpse(&enable_httpse_);
   chrome_network_delegate->set_enable_tracking_protection(&enable_tracking_protection_);
   chrome_network_delegate->set_enable_ad_block(&enable_ad_block_);
   chrome_network_delegate->set_force_google_safe_search(
@@ -1423,6 +1425,7 @@ void ProfileIOData::ShutdownOnUIThread(
     dice_enabled_->Destroy();
   enable_referrers_.Destroy();
   enable_do_not_track_.Destroy();
+  enable_httpse_.Destroy();
   enable_tracking_protection_.Destroy();
   enable_ad_block_.Destroy();
   force_google_safesearch_.Destroy();
