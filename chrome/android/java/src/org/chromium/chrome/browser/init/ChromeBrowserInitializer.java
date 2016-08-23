11bf4dc409110fd698d69144f60e592eaa64668d
Serg <serg.zhukovsky@gmail.com>
2016-08-22 22:56:57 -0400

    Added Bravery Panel with top switch
    Auditors: @bbondy
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
    	chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarTablet.java
    	chrome/android/java/strings/android_chrome_strings.grd
    	chrome/browser/android/chrome_jni_registrar.cc
    	chrome/chrome_browser.gypi
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
    	chrome/android/java/src/org/chromium/chrome/browser/tabmodel/ChromeTabCreator.java
    	chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarTablet.java
    	chrome/android/java_sources.gni
    	chrome/chrome_browser.gypi
    
    Conflicts:
    	chrome/android/java/res/layout-sw600dp/toolbar.xml
    	chrome/android/java/res/layout/toolbar.xml
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java

--- chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
@@ -485,6 +485,11 @@ public class ChromeBrowserInitializer {
                 AsyncTask.THREAD_POOL_EXECUTOR.execute(new LogcatExtractionRunnable(minidump));
             }
         });
+
+        ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
+        if (null != app) {
+            app.initShieldsConfig();
+        }
     }
 
     private void waitForDebuggerIfNeeded() {
