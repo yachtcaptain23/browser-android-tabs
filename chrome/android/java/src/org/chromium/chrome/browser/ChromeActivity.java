26077ac290dd795754dcdc29d44c7059796fbb22
Serg <serg.zhukovsky@gmail.com>
2016-08-22 22:56:57 -0400

    Added Bravery Panel with top switch
    Auditors: @bbondy
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
    	chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarTablet.java
    	chrome/android/java/strings/android_chrome_strings.grd
    	chrome/browser/android/chrome_jni_registrar.cc
    	chrome/chrome_browser.gypi
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
    	chrome/android/java/src/org/chromium/chrome/browser/tabmodel/ChromeTabCreator.java
    	chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarTablet.java
    	chrome/android/java_sources.gni
    	chrome/chrome_browser.gypi
    
    Conflicts:
    	chrome/android/java/res/layout-sw600dp/toolbar.xml
    	chrome/android/java/res/layout/toolbar.xml
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -38,6 +38,9 @@ import android.view.WindowManager;
 import android.view.accessibility.AccessibilityManager;
 import android.view.accessibility.AccessibilityManager.AccessibilityStateChangeListener;
 import android.view.accessibility.AccessibilityManager.TouchExplorationStateChangeListener;
+import android.widget.ImageButton;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 
 import org.chromium.base.ActivityState;
 import org.chromium.base.ApiCompatibilityUtils;
@@ -58,7 +61,9 @@ import org.chromium.chrome.browser.IntentHandler.TabOpenType;
 import org.chromium.chrome.browser.appmenu.AppMenu;
 import org.chromium.chrome.browser.appmenu.AppMenuHandler;
 import org.chromium.chrome.browser.appmenu.AppMenuObserver;
+import org.chromium.chrome.browser.appmenu.BraveShieldsMenuObserver;
 import org.chromium.chrome.browser.appmenu.AppMenuPropertiesDelegate;
+import org.chromium.chrome.browser.appmenu.BraveShieldsMenuHandler;
 import org.chromium.chrome.browser.bookmarks.BookmarkModel;
 import org.chromium.chrome.browser.bookmarks.BookmarkUtils;
 import org.chromium.chrome.browser.compositor.CompositorViewHolder;
@@ -266,6 +271,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
     private DataReductionPromoSnackbarController mDataReductionPromoSnackbarController;
     private AppMenuPropertiesDelegate mAppMenuPropertiesDelegate;
     private AppMenuHandler mAppMenuHandler;
+    private BraveShieldsMenuHandler mBraveShieldsMenuHandler;
     private ToolbarManager mToolbarManager;
     private BottomSheet mBottomSheet;
     private BottomSheetContentController mBottomSheetContentController;
@@ -520,6 +526,28 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
                 onOmniboxFocusChanged(hasFocus);
             }
         };
+        mBraveShieldsMenuHandler = new BraveShieldsMenuHandler(this, R.menu.brave_shields_menu);
+        mBraveShieldsMenuHandler.addObserver(new BraveShieldsMenuObserver() {
+            @Override
+            public void onMenuTopShieldsChanged(boolean isOn) {
+                if (isOn) {
+                    setBraveShieldsColored();
+                } else {
+                    setBraveShieldsBlackAndWhite();
+                }
+                Tab currentTab = getActivityTab();
+                if (currentTab == null) {
+                    return;
+                }
+                if (currentTab.isLoading()) {
+                    currentTab.stopLoading();
+                } else {
+                    currentTab.reload();
+                    RecordUserAction.record("MobileToolbarReload");
+                }
+                if (mBraveShieldsMenuHandler != null) mBraveShieldsMenuHandler.hideBraveShieldsMenu();
+            }
+        });
         mToolbarManager = new ToolbarManager(this, toolbarContainer, mAppMenuHandler,
                 mAppMenuPropertiesDelegate, getCompositorViewHolder().getInvalidator(),
                 urlFocusChangedCallback);
@@ -546,6 +574,19 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             @Override
             public void onMenuHighlightChanged(boolean highlighting) {}
         });
+        setBraveShieldsBlackAndWhite();
+    }
+
+    protected void setBraveShieldsBlackAndWhite() {
+        ColorMatrix matrix = new ColorMatrix();
+        matrix.setSaturation(0);
+
+        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
+        ((ImageButton)findViewById(R.id.brave_shields_button)).setColorFilter(filter);
+    }
+
+    protected void setBraveShieldsColored() {
+        ((ImageButton)findViewById(R.id.brave_shields_button)).clearColorFilter();
     }
 
     /**
@@ -912,7 +953,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
         Tab tab = getActivityTab();
         if (tab != null && !hasWindowFocus()) tab.onActivityHidden();
         if (mAppMenuHandler != null) mAppMenuHandler.hideAppMenu();
-
+        if (null != mBraveShieldsMenuHandler) mBraveShieldsMenuHandler.hideBraveShieldsMenu();
         if (GSAState.getInstance(this).isGsaAvailable() && !SysUtils.isLowEndDevice()) {
             GSAAccountChangeListener.getInstance().disconnect();
             if (mSyncStateChangedListener != null) {
@@ -1720,6 +1761,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         if (mAppMenuHandler != null) mAppMenuHandler.hideAppMenu();
+        if (null != mBraveShieldsMenuHandler) mBraveShieldsMenuHandler.hideBraveShieldsMenu();
         super.onConfigurationChanged(newConfig);
 
         // We only handle VR UI mode changes. Any other changes should follow the default behavior
@@ -1856,6 +1898,14 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
         tabCreator.createNewTab(
                 new LoadUrlParams(searchUrl, PageTransition.LINK),
                 TabModel.TabLaunchType.FROM_LINK, getActivityTab());
+    }
+
+    /**
+     * @return The {@link AppMenuHandler} associated with this activity.
+     */
+    @VisibleForTesting
+    public BraveShieldsMenuHandler getBraveShieldsMenuHandler() {
+        return mBraveShieldsMenuHandler;
     }
 
     /**
--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -34,6 +34,7 @@ import android.widget.FrameLayout;
 import android.widget.PopupWindow.OnDismissListener;
 
 import org.chromium.base.ActivityState;
+import org.chromium.base.ContextUtils;
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.Callback;
@@ -110,6 +111,8 @@ import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModel.TabLaunchType;
 import org.chromium.chrome.browser.tabmodel.TabModel.TabSelectionType;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+import org.chromium.chrome.browser.tabmodel.TabModel.TabSelectionType;
+import org.chromium.chrome.browser.tabmodel.TabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorImpl;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabObserver;
@@ -145,6 +148,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.net.URL;
 
 /**
  * This is the main activity for ChromeMobile when not running in document mode.  All the tabs
@@ -393,6 +397,17 @@ public class ChromeTabbedActivity
             mTabModelSelectorImpl.onNativeLibraryReady(getTabContentManager());
 
             mTabModelObserver = new TabModelSelectorTabModelObserver(mTabModelSelectorImpl) {
+                @Override
+                public void didSelectTab(Tab tab, TabSelectionType type, int lastId) {
+                    try {
+                        URL url = new URL(tab.getUrl());
+
+                        setBraveShieldsColor(url.getHost());
+                    } catch (Exception e) {
+                        setBraveShieldsBlackAndWhite();
+                    }
+                }
+
                 @Override
                 public void didCloseTab(int tabId, boolean incognito) {
                     closeIfNoTabsAndHomepageEnabled(false);
@@ -758,6 +773,26 @@ public class ChromeTabbedActivity
                     toggleOverview();
                 }
             };
+            OnClickListener braveShieldsClickHandler = new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (getFullscreenManager() != null
+                            && getFullscreenManager().getPersistentFullscreenMode()) {
+                        return;
+                    }
+                    Tab currentTab = getActivityTab();
+                    if (currentTab != null) {
+                        try {
+                            URL url = new URL(currentTab.getUrl());
+
+                            setBraveShieldsColor(url.getHost());
+                            getBraveShieldsMenuHandler().show((View)findViewById(R.id.brave_shields_button), url.getHost());
+                        } catch (Exception e) {
+                            setBraveShieldsBlackAndWhite();
+                        }
+                    }
+                }
+            };
             OnClickListener newTabClickHandler = new OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -778,7 +813,8 @@ public class ChromeTabbedActivity
             getToolbarManager().initializeWithNative(mTabModelSelectorImpl,
                     getFullscreenManager().getBrowserVisibilityDelegate(),
                     mFindToolbarManager, mLayoutManager, mLayoutManager,
-                    tabSwitcherClickHandler, newTabClickHandler, bookmarkClickHandler, null);
+                    tabSwitcherClickHandler, newTabClickHandler, bookmarkClickHandler,
+                    braveShieldsClickHandler, null);
 
             if (isTablet()) {
                 EmptyBackgroundViewWrapper bgViewWrapper = new EmptyBackgroundViewWrapper(
@@ -1447,6 +1483,33 @@ public class ChromeTabbedActivity
                 } else {
                     mIsFirstPageLoadStart = false;
                 }
+
+                ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
+                if ((null != app) && (null != app.getShieldsConfig())) {
+                    app.getShieldsConfig().setTabModelSelectorTabObserver(mTabModelSelectorTabObserver);
+                }
+                if (getActivityTab() == tab) {
+                    try {
+                        URL urlCheck = new URL(url);
+                        setBraveShieldsColor(urlCheck.getHost());
+                    } catch (Exception e) {
+                        setBraveShieldsBlackAndWhite();
+                    }
+                }
+                tab.clearBraveShieldsCount();
+            }
+
+            @Override
+            public void onPageLoadFinished(Tab tab) {
+                String url = tab.getUrl();
+                if (getActivityTab() == tab) {
+                    try {
+                        URL urlCheck = new URL(url);
+                        setBraveShieldsColor(urlCheck.getHost());
+                    } catch (Exception e) {
+                        setBraveShieldsBlackAndWhite();
+                    }
+                }
             }
 
             @Override
@@ -1559,6 +1622,18 @@ public class ChromeTabbedActivity
                 LauncherShortcutActivity.updateIncognitoShortcut(ChromeTabbedActivity.this);
             }
         });
+    }
+
+    private void setBraveShieldsColor(String url) {
+        ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
+        if (null != app) {
+            if (app.getShieldsConfig().isTopShieldsEnabled(url)) {
+                // Set Brave Shields button in color if we have a valid URL
+                setBraveShieldsColored();
+            } else {
+                setBraveShieldsBlackAndWhite();
+            }
+        }
     }
 
     @Override
@@ -2059,6 +2134,7 @@ public class ChromeTabbedActivity
         if (mFindToolbarManager != null) mFindToolbarManager.hideToolbar();
         if (getAssistStatusHandler() != null) getAssistStatusHandler().updateAssistState();
         if (getAppMenuHandler() != null) getAppMenuHandler().hideAppMenu();
+        if (getBraveShieldsMenuHandler() != null) getBraveShieldsMenuHandler().hideBraveShieldsMenu();
         ApiCompatibilityUtils.setStatusBarColor(getWindow(), Color.BLACK);
         StartupMetrics.getInstance().recordOpenedTabSwitcher();
     }
@@ -2069,6 +2145,7 @@ public class ChromeTabbedActivity
     @Override
     public void onOverviewModeStartedHiding(boolean showToolbar, boolean delayAnimation) {
         if (getAppMenuHandler() != null) getAppMenuHandler().hideAppMenu();
+        if (getBraveShieldsMenuHandler() != null) getBraveShieldsMenuHandler().hideBraveShieldsMenu();
     }
 
     @Override
