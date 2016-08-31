1e7fd9574bc578bc0fc31f19c574ed5668132816
Serg <serg.zhukovsky@gmail.com>
2016-08-30 20:48:07 -0400

    Added blocking statistic
    Auditors: @bbondy
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java

--- chrome/android/java/src/org/chromium/chrome/browser/tab/EmptyTabObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/EmptyTabObserver.java
@@ -131,4 +131,7 @@ public class EmptyTabObserver implements TabObserver {
 
     @Override
     public void onReparentingFinished(Tab tab) { }
+
+    @Override
+    public void onBraveShieldsCountUpdate(String url, int adsAndTrackers, int httpsUpgrades) { }
 }
