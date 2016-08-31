329dfde9474dd7bda942d7b1c809d588ef237b55
Serg <serg.zhukovsky@gmail.com>
2016-08-30 20:48:07 -0400

    Added blocking statistic
    Auditors: @bbondy
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java

--- chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
@@ -413,6 +413,8 @@ public class Tab
     private int mTopControlsHeight;
     private int mBottomControlsHeight;
     private boolean mControlsResizeView;
+    private int mAdsAndTrackers;
+    private int mHttpsUpgrades;
 
     private GestureStateListener createGestureStateListener() {
         return new GestureStateListener() {
@@ -620,6 +622,8 @@ public class Tab
                 updateInteractableState();
             }
         };
+        mAdsAndTrackers = 0;
+        mHttpsUpgrades = 0;
     }
 
     private int calculateDefaultThemeColor() {
@@ -3406,6 +3410,23 @@ public class Tab
         Tracker tracker = TrackerFactory.getTrackerForProfile(Profile.getLastUsedProfile());
         tracker.dismissed(FeatureConstants.MEDIA_DOWNLOAD_FEATURE);
         nativeMediaDownloadInProductHelpDismissed(mNativeTabAndroid);
+
+    public void braveShieldsCountUpdate(int adsAndTrackers, int httpsUpgrades) {
+        mAdsAndTrackers += adsAndTrackers;
+        mHttpsUpgrades += httpsUpgrades;
+    }
+
+    public int getAdsAndTrackers() {
+        return mAdsAndTrackers;
+    }
+
+    public int getHttpsUpgrades() {
+        return mHttpsUpgrades;
+    }
+
+    public void clearBraveShieldsCount() {
+        mAdsAndTrackers = 0;
+        mHttpsUpgrades = 0;
     }
 
     private native void nativeInit();
