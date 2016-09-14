48ca232dbf8346e6c8353203434fb9729f10fa55
Serg <serg.zhukovsky@gmail.com>
2016-09-14 09:41:46 -0400

    Added Scripts Blocked statistic
    Auditors: @bbondy
    
    Conflicts:
    	components/content_settings/content/common/content_settings_messages.h
    	third_party/WebKit/Source/bindings/core/v8/ScriptController.cpp
    	third_party/WebKit/Source/core/dom/ScriptLoader.cpp
    	third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
    	third_party/WebKit/Source/core/loader/FrameLoaderClient.h
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.h
    	third_party/WebKit/public/web/WebContentSettingsClient.h
    
    Conflicts:
    	chrome/browser/content_settings/tab_specific_content_settings.cc
    	chrome/renderer/content_settings_observer.h
    	third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/public/web/WebContentSettingsClient.h

--- third_party/WebKit/Source/core/exported/LocalFrameClientImpl.cpp
+++ third_party/WebKit/Source/core/exported/LocalFrameClientImpl.cpp
@@ -290,6 +290,11 @@ bool LocalFrameClientImpl::HasWebView() const {
   return web_frame_->ViewImpl();
 }
 
+void LocalFrameClientImpl::DeniedScript()
+{
+  GetContentSettingsClient()->DeniedScript();
+}
+
 bool LocalFrameClientImpl::InShadowTree() const {
   return web_frame_->InShadowTree();
 }
