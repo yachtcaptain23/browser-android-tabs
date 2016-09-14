c71d823035f25af174d46200adf8d3dc0b5f7e87
Serg <serg.zhukovsky@gmail.com>
2016-09-14 09:41:46 -0400

    Added Scripts Blocked statistic
    Auditors: @bbondy
    
    Conflicts:
    	components/content_settings/content/common/content_settings_messages.h
    	third_party/WebKit/Source/bindings/core/v8/ScriptController.cpp
    	third_party/WebKit/Source/core/dom/ScriptLoader.cpp
    	third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
    	third_party/WebKit/Source/core/loader/FrameLoaderClient.h
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.h
    	third_party/WebKit/public/web/WebContentSettingsClient.h
    
    Conflicts:
    	chrome/browser/content_settings/tab_specific_content_settings.cc
    	chrome/renderer/content_settings_observer.h
    	third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/public/web/WebContentSettingsClient.h

--- third_party/WebKit/Source/web/LocalFrameClientImpl.h
+++ third_party/WebKit/Source/web/LocalFrameClientImpl.h
@@ -185,6 +185,7 @@ class LocalFrameClientImpl final : public LocalFrameClient {
   void DidAddContentSecurityPolicies(
       const blink::WebVector<WebContentSecurityPolicy>&) override;
   void DidChangeFrameOwnerProperties(HTMLFrameOwnerElement*) override;
+  void DeniedScript() override;
 
   void DispatchWillStartUsingPeerConnectionHandler(
       WebRTCPeerConnectionHandler*) override;
