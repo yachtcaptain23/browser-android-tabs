0be3a5b9659f6d3da44c57fc98ed201ba8ca37cc
Serg <serg.zhukovsky@gmail.com>
2016-09-20 15:53:53 -0400

    Added Brave Shields to custom tab
    Auditors: @bbondy
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1294,6 +1294,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements OverviewMode
                 if ((null != app) && (null != app.getShieldsConfig())) {
                     app.getShieldsConfig().setTabModelSelectorTabObserver(mTabModelSelectorTabObserver);
                 }
+
                 if (getActivityTab() == tab) {
                     try {
                         URL urlCheck = new URL(url);
