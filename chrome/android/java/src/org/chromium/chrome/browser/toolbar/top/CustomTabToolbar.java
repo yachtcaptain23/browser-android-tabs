a53ad8a64fbbf2903d64a62a4c90ed51ef74651f
Serg <serg.zhukovsky@gmail.com>
2016-09-20 15:53:53 -0400

    Added Brave Shields to custom tab
    Auditors: @bbondy
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/CustomTabToolbar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/CustomTabToolbar.java
@@ -36,6 +36,7 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.chromium.base.ApiCompatibilityUtils;
@@ -70,6 +71,7 @@ import org.chromium.ui.interpolators.BakedBezierInterpolator;
 import org.chromium.ui.text.SpanApplier;
 import org.chromium.ui.text.SpanApplier.SpanInfo;
 import org.chromium.ui.widget.Toast;
+import org.chromium.base.Log;
 
 import java.util.List;
 import java.util.regex.Pattern;
@@ -146,6 +148,8 @@ public class CustomTabToolbar
     private String mFirstUrl;
 
     protected ToolbarDataProvider mToolbarDataProvider;
+    private OnClickListener mBraveShieldsListener;
+    private ImageView mBraveShieldsButton;
 
     private Runnable mTitleAnimationStarter = new Runnable() {
         @Override
@@ -181,6 +185,15 @@ public class CustomTabToolbar
         mCustomActionButtons = findViewById(R.id.action_buttons);
         mCloseButton = findViewById(R.id.close_button);
         mCloseButton.setOnLongClickListener(this);
+        mBraveShieldsButton = (ImageView) findViewById(R.id.brave_shields_button);
+        mBraveShieldsButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mBraveShieldsButton && null != mBraveShieldsListener) {
+                    mBraveShieldsListener.onClick(mBraveShieldsButton);
+                }
+            }
+        });
         mAnimDelegate = new CustomTabToolbarAnimationDelegate(mSecurityButton, mTitleUrlContainer);
     }
 
@@ -235,6 +248,11 @@ public class CustomTabToolbar
                 mCustomActionButtons.getChildCount() - 1 - index);
         assert button != null;
         updateCustomActionButtonVisuals(button, drawable, description);
+    }
+
+    @Override
+    public void setBraveShieldsClickHandler(OnClickListener listener) {
+        mBraveShieldsListener = listener;
     }
 
     private void updateCustomActionButtonVisuals(
