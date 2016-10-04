e7ea16aa4702935a6a9d089f9a177fbd622d42ee
Serg <serg.zhukovsky@gmail.com>
2016-10-04 13:46:17 -0400

    Added Block 3rd party cookies switch
    Auditors: @bbondy
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
    	components/content_settings/core/browser/cookie_settings.cc
    	components/content_settings/core/browser/cookie_settings.h

--- components/signin/core/browser/signin_header_helper.cc
+++ components/signin/core/browser/signin_header_helper.cc
@@ -51,7 +51,7 @@ DiceResponseParams::SignoutInfo::~SignoutInfo() {}
 DiceResponseParams::SignoutInfo::SignoutInfo(const SignoutInfo&) = default;
 
 bool SettingsAllowSigninCookies(
-    const content_settings::CookieSettings* cookie_settings) {
+    content_settings::CookieSettings* cookie_settings) {
   GURL gaia_url = GaiaUrls::GetInstance()->gaia_url();
   GURL google_url = GaiaUrls::GetInstance()->google_url();
   return cookie_settings &&
@@ -62,7 +62,7 @@ bool SettingsAllowSigninCookies(
 std::string BuildMirrorRequestCookieIfPossible(
     const GURL& url,
     const std::string& account_id,
-    const content_settings::CookieSettings* cookie_settings,
+    content_settings::CookieSettings* cookie_settings,
     int profile_mode_mask) {
   return signin::ChromeConnectedHeaderHelper::BuildRequestCookieIfPossible(
       url, account_id, cookie_settings, profile_mode_mask);
@@ -114,7 +114,7 @@ SigninHeaderHelper::ParseAccountConsistencyResponseHeader(
 
 bool SigninHeaderHelper::ShouldBuildRequestHeader(
     const GURL& url,
-    const content_settings::CookieSettings* cookie_settings) {
+    content_settings::CookieSettings* cookie_settings) {
   // If signin cookies are not allowed, don't add the header.
   if (!SettingsAllowSigninCookies(cookie_settings))
     return false;
@@ -132,7 +132,7 @@ void AppendOrRemoveAccountConsistencyRequestHeader(
     const std::string& account_id,
     bool sync_enabled,
     bool sync_has_auth_error,
-    const content_settings::CookieSettings* cookie_settings,
+    content_settings::CookieSettings* cookie_settings,
     int profile_mode_mask) {
   const GURL& url = redirect_url.is_empty() ? request->url() : redirect_url;
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
