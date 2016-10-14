0de1cc7e14d7266133494a15ee665fe595225e24
Serg <serg.zhukovsky@gmail.com>
2016-10-14 10:29:29 -0400

    Don't close Bravery Panel when tap on switch
    Auditors: @bbondy

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -663,7 +663,10 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
                     }
                     currentTab.reloadIgnoringCache();
                     RecordUserAction.record("MobileToolbarReload");
-                    if (mBraveShieldsMenuHandler != null) mBraveShieldsMenuHandler.hideBraveShieldsMenu();
+                    if (null != mBraveShieldsMenuHandler) {
+                        // Clean the Bravery Panel
+                        mBraveShieldsMenuHandler.updateValues(0, 0, 0);
+                    }
                 }
             });
             mToolbarManager = new ToolbarManager(this, toolbarContainer, mAppMenuHandler,
