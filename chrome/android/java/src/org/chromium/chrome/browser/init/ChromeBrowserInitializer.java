3899c73c47cd5bd695d822fee66cf885e896d2b7
Serg <serg.zhukovsky@gmail.com>
2016-10-17 14:57:51 -0400

    Added more logs

--- chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
@@ -111,7 +111,10 @@ public class ChromeBrowserInitializer {
     }
 
     private void InitAdBlock() {
+      Log.i(TAG, "Init AdBlock");
       if (mAdBlockInitCalled) {
+          Log.i(TAG, "Init AdBlock was already called");
+
           return;
       }
       mAdBlockInitCalled = true;
@@ -121,6 +124,7 @@ public class ChromeBrowserInitializer {
       new DownloadAdBlockDataAsyncTask().execute();
       new DownloadHTTPSDataAsyncTask().execute();
       PrefServiceBridge.getInstance().setBlockThirdPartyCookiesEnabled(true);
+      Log.i(TAG, "Started AdBlock async tasks");
     }
 
     // Tracking ptotection data download
