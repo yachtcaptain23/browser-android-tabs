184e6e997cce2fb4ff6c88131416c65d91bb5052
Serg <serg.zhukovsky@gmail.com>
2016-10-18 13:35:11 -0400

    Removed Google logo when it is a default Search Engine
    Auditors: @bbondy
    
    Conflicts:
    	chrome/android/java/res/layout/new_tab_page_layout.xml
    	chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageLayout.java
    	chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -91,7 +91,7 @@ public class NewTabPageView extends FrameLayout implements TileGroup.Observer {
     private NewTabPageRecyclerView mRecyclerView;
 
     private NewTabPageLayout mNewTabPageLayout;
-    private LogoView mSearchProviderLogoView;
+    //private LogoView mSearchProviderLogoView;
     private View mSearchBoxView;
     private ImageView mVoiceSearchButton;
     private SiteSectionViewHolder mSiteSectionViewHolder;
@@ -265,7 +265,7 @@ public class NewTabPageView extends FrameLayout implements TileGroup.Observer {
                 SiteSection.createViewHolder(mNewTabPageLayout.getSiteSectionView());
         mSiteSectionViewHolder.bindDataSource(mTileGroup, tileRenderer);
 
-        mSearchProviderLogoView = mNewTabPageLayout.findViewById(R.id.search_provider_logo);
+        /*mSearchProviderLogoView = mNewTabPageLayout.findViewById(R.id.search_provider_logo);
         int experimentalLogoHeightDp = ChromeFeatureList.getFieldTrialParamByFeatureAsInt(
                 ChromeFeatureList.NTP_CONDENSED_LAYOUT, PARAM_CONDENSED_LAYOUT_LOGO_HEIGHT, 0);
         if (experimentalLogoHeightDp > 0) {
@@ -274,7 +274,7 @@ public class NewTabPageView extends FrameLayout implements TileGroup.Observer {
             mSearchProviderLogoView.setLayoutParams(logoParams);
         }
         mLogoDelegate = new LogoDelegateImpl(
-                mManager.getNavigationDelegate(), mSearchProviderLogoView, profile);
+                mManager.getNavigationDelegate(), mSearchProviderLogoView, profile);*/
 
         mSearchBoxView = mNewTabPageLayout.findViewById(R.id.search_box);
         mNoSearchLogoSpacer = mNewTabPageLayout.findViewById(R.id.no_search_logo_spacer);
@@ -603,10 +603,9 @@ public class NewTabPageView extends FrameLayout implements TileGroup.Observer {
             @Override
             public void onLogoAvailable(Logo logo, boolean fromCache) {
                 if (logo == null && fromCache) return;
-
-                mSearchProviderLogoView.setDelegate(mLogoDelegate);
-                mSearchProviderLogoView.updateLogo(logo);
-                mSnapshotTileGridChanged = true;
+                //mSearchProviderLogoView.setDelegate(mLogoDelegate);
+                //mSearchProviderLogoView.updateLogo(logo);
+                mSnapshotMostVisitedChanged = true;
             }
         });
     }
@@ -732,7 +731,7 @@ public class NewTabPageView extends FrameLayout implements TileGroup.Observer {
      * @param alpha opacity (alpha) value to use.
      */
     public void setSearchProviderLogoAlpha(float alpha) {
-        mSearchProviderLogoView.setAlpha(alpha);
+        //mSearchProviderLogoView.setAlpha(alpha);
     }
 
     /**
@@ -837,7 +836,7 @@ public class NewTabPageView extends FrameLayout implements TileGroup.Observer {
      *         InvalidationAwareThumbnailProvider#captureThumbnail(Canvas)
      */
     void captureThumbnail(Canvas canvas) {
-        mSearchProviderLogoView.endFadeAnimation();
+        //mSearchProviderLogoView.endFadeAnimation();
         ViewUtils.captureBitmap(this, canvas);
         mSnapshotWidth = getWidth();
         mSnapshotHeight = getHeight();
