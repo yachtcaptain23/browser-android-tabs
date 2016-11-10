3c22d3bb8075e4c645adf117564ef1fa280b240c
Serg <serg.zhukovsky@gmail.com>
2016-11-10 14:22:11 -0500

    Added Canvas and WebGL fingerprinting protection
    Auditors: @bbondy
    
    Conflicts:
    	chrome/common/render_messages.h
    	components/content_settings/content/common/content_settings_messages.h
    	third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
    	third_party/WebKit/Source/core/loader/FrameLoaderClient.h
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.h
    	third_party/WebKit/public/web/WebContentSettingsClient.h
    
    Conflicts:
    	chrome/browser/renderer_host/chrome_render_message_filter.cc
    	chrome/browser/renderer_host/chrome_render_message_filter.h
    
    Conflicts:
    	chrome/browser/content_settings/tab_specific_content_settings.cc
    	chrome/browser/renderer_host/chrome_render_message_filter.h
    	chrome/renderer/content_settings_observer.h
    	third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/public/web/WebContentSettingsClient.h

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -412,8 +412,8 @@ public class CustomTabActivity extends ChromeActivity {
 
             @Override
             public void onBraveShieldsCountUpdate(String url, int adsAndTrackers, int httpsUpgrades,
-                    int scriptsBlocked) {
-                braveShieldsCountUpdate(url, adsAndTrackers, httpsUpgrades, scriptsBlocked);
+                    int scriptsBlocked, int fingerprintsBlocked) {
+                braveShieldsCountUpdate(url, adsAndTrackers, httpsUpgrades, scriptsBlocked, fingerprintsBlocked);
             }
         };
 
@@ -540,7 +540,8 @@ public class CustomTabActivity extends ChromeActivity {
                           , url.getHost()
                           , currentTab.getAdsAndTrackers()
                           , currentTab.getHttpsUpgrades()
-                          , currentTab.getScriptsBlocked());
+                          , currentTab.getScriptsBlocked()
+                          , currentTab.getFingerprintsBlocked());
                     } catch (Exception e) {
                         setBraveShieldsBlackAndWhite();
                     }
