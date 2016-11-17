680ebe49de4ea9f822095bf84f2a241faae8a803
Serg <serg.zhukovsky@gmail.com>
2016-11-16 22:44:06 -0500

    Fixed fingerprints methos counts
    Conflicts:
    	third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
    	third_party/WebKit/Source/core/html/HTMLCanvasElement.h
    	third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.cpp
    
    Conflicts:
    	third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -3497,6 +3497,17 @@ WebGLRenderingContextBase::getSupportedExtensions() {
   if (isContextLost())
     return base::nullopt;
 
+  LocalFrame* frame = canvas()->document().frame();
+  bool allowed = true;
+  if (frame) {
+      allowed = frame->loader().client()->allowFingerprinting();
+      if (!allowed && !canvas()->wasBlockedByFingerprinting()) {
+          frame->loader().client()->deniedFingerprinting();
+      }
+  }
+  if (!allowed)
+      return nullptr;
+
   Vector<String> result;
 
   for (size_t i = 0; i < extensions_.size(); ++i) {
