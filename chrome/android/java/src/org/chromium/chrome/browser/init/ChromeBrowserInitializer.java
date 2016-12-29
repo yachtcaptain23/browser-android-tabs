229c69e4202b7ebd9a9d0a254a831478f9a4802c
Serg <serg.zhukovsky@gmail.com>
2016-12-29 11:03:44 -0500

    Added extra checks on abblock libs
    Auditors: @bbondy

--- chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
@@ -246,6 +246,8 @@ public class ChromeBrowserInitializer {
         };
         handlePreNativeStartup(parts);
         handlePostNativeStartup(false, parts);
+        InitAdBlock();
+        UpdateStats();
     }
 
     /**
@@ -376,8 +378,6 @@ public class ChromeBrowserInitializer {
             @Override
             public void initFunction() {
                 ProcessInitializationHandler.getInstance().initializePostNative();
-                InitAdBlock();
-                UpdateStats();
             }
         });
 
