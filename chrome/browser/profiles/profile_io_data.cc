5719d6ac08b3e317f828b2629da92ab298ffcc44
Serg <serg.zhukovsky@gmail.com>
2017-01-12 15:44:17 -0500

    Added regional adblock
    Auditors: @bbondy
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
    	chrome/common/pref_names.cc
    	chrome/common/pref_names.h

--- chrome/browser/profiles/profile_io_data.cc
+++ chrome/browser/profiles/profile_io_data.cc
@@ -468,6 +468,7 @@ void ProfileIOData::InitializeOnUIThread(Profile* profile) {
       &enable_httpse_,
       &enable_tracking_protection_,
       &enable_ad_block_,
+      &enable_ad_block_regional_,
       &force_google_safesearch_,
       &force_youtube_restrict_,
       &allowed_domains_for_apps_,
@@ -1026,6 +1027,7 @@ void ProfileIOData::Init(
   network_delegate->set_enable_httpse(&enable_httpse_);
   network_delegate->set_enable_tracking_protection(&enable_tracking_protection_);
   network_delegate->set_enable_ad_block(&enable_ad_block_);
+  network_delegate->set_enable_ad_block_regional(&enable_ad_block_regional_);
   network_delegate->set_force_google_safe_search(&force_google_safesearch_);
   network_delegate->set_force_youtube_restrict(&force_youtube_restrict_);
   network_delegate->set_allowed_domains_for_apps(&allowed_domains_for_apps_);
@@ -1240,6 +1242,7 @@ void ProfileIOData::ShutdownOnUIThread(
   enable_httpse_.Destroy();
   enable_tracking_protection_.Destroy();
   enable_ad_block_.Destroy();
+  enable_ad_block_regional_.Destroy();
   force_google_safesearch_.Destroy();
   force_youtube_restrict_.Destroy();
   allowed_domains_for_apps_.Destroy();
