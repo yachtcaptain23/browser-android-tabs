4b065dae791d375ff856fb58185d60678351c50d
Serg <serg.zhukovsky@gmail.com>
2017-01-27 16:38:49 -0500

    Disabled quic
    Auditors: @bbondy

--- components/network_session_configurator/browser/network_session_configurator.cc
+++ components/network_session_configurator/browser/network_session_configurator.cc
@@ -43,8 +43,8 @@ const char kTCPFastOpenFieldTrialName[] = "TCPFastOpen";
 const char kTCPFastOpenHttpsEnabledGroupName[] = "HttpsEnabled";
 
 const char kQuicFieldTrialName[] = "QUIC";
-const char kQuicFieldTrialEnabledGroupName[] = "Enabled";
-const char kQuicFieldTrialHttpsEnabledGroupName[] = "HttpsEnabled";
+//const char kQuicFieldTrialEnabledGroupName[] = "Enabled";
+//const char kQuicFieldTrialHttpsEnabledGroupName[] = "HttpsEnabled";
 
 const char kHttp2FieldTrialName[] = "HTTP2";
 
@@ -168,7 +168,7 @@ bool ShouldEnableQuic(base::StringPiece quic_trial_group,
                       const VariationParameters& quic_trial_params,
                       bool is_quic_force_disabled,
                       bool is_quic_force_enabled) {
-  if (is_quic_force_disabled)
+  /*if (is_quic_force_disabled)
     return false;
   if (is_quic_force_enabled)
     return true;
@@ -176,7 +176,8 @@ bool ShouldEnableQuic(base::StringPiece quic_trial_group,
   return quic_trial_group.starts_with(kQuicFieldTrialEnabledGroupName) ||
          quic_trial_group.starts_with(kQuicFieldTrialHttpsEnabledGroupName) ||
          base::LowerCaseEqualsASCII(
-             GetVariationParam(quic_trial_params, "enable_quic"), "true");
+             GetVariationParam(quic_trial_params, "enable_quic"), "true");*/
+  return false;
 }
 
 bool ShouldMarkQuicBrokenWhenNetworkBlackholes(
