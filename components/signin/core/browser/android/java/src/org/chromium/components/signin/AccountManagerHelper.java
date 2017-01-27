5311d58d51fae595034bece07295e4fbb3fb3707
Serg <serg.zhukovsky@gmail.com>
2017-01-27 09:47:15 -0500

    Commented unneeded code

--- components/signin/core/browser/android/java/src/org/chromium/components/signin/AccountManagerHelper.java
+++ components/signin/core/browser/android/java/src/org/chromium/components/signin/AccountManagerHelper.java
@@ -394,7 +394,7 @@ public class AccountManagerHelper {
          */
         private void attempt() {
             // Clear any transient error.
-            mIsTransientError.set(false);
+            /*mIsTransientError.set(false);
             new AsyncTask<Void, Void, T>() {
                 @Override
                 public T doInBackground(Void... params) {
@@ -420,7 +420,7 @@ public class AccountManagerHelper {
                         NetworkChangeNotifier.addConnectionTypeObserver(ConnectionRetry.this);
                     }
                 }
-            }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+            }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);*/
         }
 
         @Override
