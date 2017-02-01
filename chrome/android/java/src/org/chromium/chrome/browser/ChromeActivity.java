a749e966aa0b094f1070f215446235fedfc45abc
Serg <serg.zhukovsky@gmail.com>
2017-01-31 22:12:27 -0500

    Added exit menu item
    Conflicts:
    	chrome/android/java/res/menu/main_menu.xml
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -2108,6 +2108,8 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
                 builder.setView(DistilledPagePrefsView.create(this));
                 builder.show();
             }
+        } else if (id == R.id.exit_id) {
+            ApplicationLifetime.terminate(false);
         } else {
             return false;
         }
