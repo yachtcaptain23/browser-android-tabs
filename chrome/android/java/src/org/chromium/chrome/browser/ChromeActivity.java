741425cbd33e39dd13d5fe0220630e7de9f65b8c
Serg <serg.zhukovsky@gmail.com>
2017-01-31 22:12:27 -0500

    Added exit menu item
    Conflicts:
    	chrome/android/java/res/menu/main_menu.xml
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -2029,7 +2029,9 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             }
         } /*else if (id == R.id.help_id) {
             startHelpAndFeedback(currentTab, "MobileMenuFeedback");
-        }*/ else {
+        }*/ else if (id == R.id.exit_id) {
+            ApplicationLifetime.terminate(false);
+        } else {
             return false;
         }
         return true;
