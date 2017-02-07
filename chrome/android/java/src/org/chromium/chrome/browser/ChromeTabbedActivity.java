43028a18d8f312e3e36ff0562c02fb27792b5fd3
Serg <serg.zhukovsky@gmail.com>
2017-02-07 12:26:38 -0500

    Made it buildable after merge
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
    	chrome/android/java/strings/translations/android_chrome_strings_ar.xtb
    	components/strings/components_strings_el.xtb
    	components/strings/components_strings_mr.xtb

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1617,6 +1617,7 @@ public class ChromeTabbedActivity
             public void onPageLoadStarted(Tab tab, String url) {
                 // Discard startup navigation measurements when the user interfered and started the
                 // 2nd navigation (in activity lifetime) in parallel.
+                Log.i("TAG", "!!!here1");
                 if (!mIsFirstPageLoadStart) {
                     UmaUtils.setRunningApplicationStart(false);
                 } else {
@@ -1627,7 +1628,6 @@ public class ChromeTabbedActivity
                 if ((null != app) && (null != app.getShieldsConfig())) {
                     app.getShieldsConfig().setTabModelSelectorTabObserver(mTabModelSelectorTabObserver);
                 }
-
                 if (getActivityTab() == tab) {
                     try {
                         URL urlCheck = new URL(url);
@@ -1641,6 +1641,7 @@ public class ChromeTabbedActivity
 
             @Override
             public void onPageLoadFinished(Tab tab) {
+              Log.i("TAG", "!!!here11");
                 String url = tab.getUrl();
                 if (getActivityTab() == tab) {
                     try {
@@ -1846,7 +1847,7 @@ public class ChromeTabbedActivity
         });
     }
 
-    private void setBraveShieldsColor(String url) {
+    protected void setBraveShieldsColor(String url) {
         ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
         if (null != app) {
             if (app.getShieldsConfig().isTopShieldsEnabled(url)) {
