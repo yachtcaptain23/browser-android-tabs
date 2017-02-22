b68a0f264e71185b3dccb27ac823129d4eda26d9
Serg <serg.zhukovsky@gmail.com>
2017-02-22 09:14:15 -0500

    Removed unneeded logs

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1532,7 +1532,6 @@ public class ChromeTabbedActivity
 
             @Override
             public void onPageLoadFinished(Tab tab) {
-              Log.i("TAG", "!!!here11");
                 String url = tab.getUrl();
                 if (getActivityTab() == tab) {
                     try {
