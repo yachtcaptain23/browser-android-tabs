acbd83733ca8221aadfc276036e8cbd3873a91f8
Serg <serg.zhukovsky@gmail.com>
2017-03-17 15:18:27 -0400

    Sync functionality(not finished)
    Conflicts:
    	build/android/lint/suppressions.xml

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -56,6 +56,7 @@ import org.chromium.base.VisibleForTesting;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.BraveSyncWorker;
 import org.chromium.chrome.browser.IntentHandler.IntentHandlerDelegate;
 import org.chromium.chrome.browser.IntentHandler.TabOpenType;
 import org.chromium.chrome.browser.appmenu.AppMenu;
@@ -478,6 +479,12 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
                 mSnackbarManager.onStart();
             }
         }
+
+        // Comment sync temporary
+        /*ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
+        if (null != app) {
+            app.mBraveSyncWorker = new BraveSyncWorker(this);
+        }*/
     }
 
     @Override
@@ -1429,6 +1436,14 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             mDidAddPolicyChangeListener = false;
         }
 
+        // Stop SyncWorker
+        ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
+        if (null != app && null != app.mBraveSyncWorker) {
+            app.mBraveSyncWorker.Stop();
+            app.mBraveSyncWorker = null;
+        }
+
+
         if (mTabContentManager != null) {
             mTabContentManager.destroy();
             mTabContentManager = null;
@@ -1757,6 +1772,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         // INVALID_BOOKMARK_ID, so the code below will fall back on adding a new bookmark instead.
         // TODO(bauerb): This does not take partner bookmarks into account.
         final long bookmarkId = tabToBookmark.getUserBookmarkId();
+        final boolean bCreateBookmark = (Tab.INVALID_BOOKMARK_ID == bookmarkId);
 
         final BookmarkModel bookmarkModel = new BookmarkModel();
 
@@ -1770,7 +1786,16 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
                         isCustomTab());
                 // If a new bookmark was created, try to save an offline page for it.
                 if (newBookmarkId != null && newBookmarkId.getId() != bookmarkId) {
-                    OfflinePageUtils.saveBookmarkOffline(newBookmarkId, tabToBookmark);
+                    OfflinePageUtils.saveBookmarkOffline(newBookmarkId, tabToBookmark);                 
+                }
+                BookmarkModel newBookmarkModel = new BookmarkModel();
+                ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
+                if (null != app && null != app.mBraveSyncWorker && null != newBookmarkId && null != newBookmarkModel) {
+                    app.mBraveSyncWorker.CreateUpdateBookmark(bCreateBookmark, newBookmarkModel.getBookmarkById(newBookmarkId));
+                    newBookmarkModel.destroy();
+                }
+                if (null != newBookmarkModel) {
+                        newBookmarkModel.destroy();
                 }
             } else {
                 bookmarkModel.destroy();
