b3b963e3a5ada1a547900b6dee79b0d6abd7d2ab
Serg <serg.zhukovsky@gmail.com>
2017-03-17 15:18:27 -0400

    Sync functionality(not finished)
    Conflicts:
    	build/android/lint/suppressions.xml

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
@@ -31,6 +31,7 @@ import org.chromium.build.BuildHooksAndroid;
 import org.chromium.build.BuildHooksConfig;
 import org.chromium.chrome.browser.crash.PureJavaExceptionHandler;
 import org.chromium.chrome.browser.crash.PureJavaExceptionReporter;
+import org.chromium.chrome.browser.BraveSyncWorker;
 import org.chromium.chrome.browser.init.InvalidStartupDialog;
 import org.chromium.chrome.browser.instantapps.InstantAppsHandler;
 import org.chromium.chrome.browser.init.ShieldsConfig;
@@ -50,6 +51,8 @@ public class ChromeApplication extends Application {
 
     private DiscardableReferencePool mReferencePool;
     private ShieldsConfig mShieldsConfig;
+    // Sync worker
+    public BraveSyncWorker mBraveSyncWorker = null;
 
     // Called by the framework for ALL processes. Runs before ContentProviders are created.
     // Quirk: context.getApplicationContext() returns null during this method.
