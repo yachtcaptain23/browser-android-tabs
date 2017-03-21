c3d677e0738f1a8f68419bb1285aec2cbb66a356
Serg <serg.zhukovsky@gmail.com>
2017-03-21 16:49:21 -0400

    Made it buildable after merge

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -474,14 +474,8 @@ public class CustomTabActivity extends ChromeActivity {
         mBottomBarDelegate = new CustomTabBottomBarDelegate(this, mIntentDataProvider,
                 getFullscreenManager());
         mBottomBarDelegate.showBottomBarIfNecessary();
-    }
 
-    @Override
-    protected TabModelSelector createTabModelSelector() {
-        mTabPersistencePolicy = new CustomTabTabPersistencePolicy(
-                getTaskId(), getSavedInstanceState() != null);
-
-        return new TabModelSelectorImpl(this, this, mTabPersistencePolicy, false, false);
+        return  tabModelSelectorImpl;
     }
 
     @Override
@@ -806,7 +800,9 @@ public class CustomTabActivity extends ChromeActivity {
         setActiveContentHandler(null);
         if (mIsClosing) {
             getTabModelSelector().closeAllTabs(true);
-            mTabPersistencePolicy.deleteMetadataStateFileAsync();
+            if (null != mTabPersistencePolicy) {
+                mTabPersistencePolicy.deleteMetadataStateFileAsync();
+            }
         } else {
             getTabModelSelector().saveState();
         }
