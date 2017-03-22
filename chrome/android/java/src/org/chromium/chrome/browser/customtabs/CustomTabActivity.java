bdc936e4ff1fad8f60193be172dc71f27745b810
Serg <serg.zhukovsky@gmail.com>
2017-03-22 18:34:48 -0400

    Fixed crash on Custom Tabs

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -431,6 +431,13 @@ public class CustomTabActivity extends ChromeActivity {
                     ColorUtils.getDarkenedColorForStatusBar(toolbarColor));
         }
 
+        // Properly attach tab's infobar to the view hierarchy, as the main tab might have been
+        // initialized prior to inflation.
+        if (mMainTab != null) {
+            ViewGroup bottomContainer = (ViewGroup) findViewById(R.id.bottom_container);
+            mMainTab.getInfoBarContainer().setParentView(bottomContainer);
+        }
+
         // Setting task title and icon to be null will preserve the client app's title and icon.
         ApiCompatibilityUtils.setTaskDescription(this, null, null, toolbarColor);
         showCustomButtonOnToolbar();
@@ -440,10 +447,10 @@ public class CustomTabActivity extends ChromeActivity {
 
     @Override
     protected TabModelSelector createTabModelSelector() {
-        TabPersistencePolicy persistencePolicy = new CustomTabTabPersistencePolicy(
+        mTabPersistencePolicy = new CustomTabTabPersistencePolicy(
                 getTaskId(), getSavedInstanceState() != null);
 
-        TabModelSelectorImpl tabModelSelectorImpl = new TabModelSelectorImpl(this, this, persistencePolicy, false, false);
+        TabModelSelectorImpl tabModelSelectorImpl = new TabModelSelectorImpl(this, this, mTabPersistencePolicy, false, false);
 
         mTabModelSelectorTabObserver = new TabModelSelectorTabObserver(tabModelSelectorImpl) {
 
@@ -453,7 +460,6 @@ public class CustomTabActivity extends ChromeActivity {
             public void onPageLoadStarted(Tab tab, String url) {
                 // Discard startup navigation measurements when the user interfered and started the
                 // 2nd navigation (in activity lifetime) in parallel.
-                Log.i("TAG", "!!!here0");
                 if (mIsFirstPageLoadStart) {
                     mIsFirstPageLoadStart = false;
                 }
@@ -474,7 +480,6 @@ public class CustomTabActivity extends ChromeActivity {
 
             @Override
             public void onPageLoadFinished(Tab tab) {
-              Log.i("TAG", "!!!here00");
                 String url = tab.getUrl();
                 if (getActivityTab() == tab) {
                     try {
@@ -491,7 +496,7 @@ public class CustomTabActivity extends ChromeActivity {
                     int scriptsBlocked, int fingerprintsBlocked) {
                 braveShieldsCountUpdate(url, adsAndTrackers, httpsUpgrades, scriptsBlocked, fingerprintsBlocked);
             }
-        };
+        }
 
         getToolbarManager().setCloseButtonDrawable(mIntentDataProvider.getCloseButtonDrawable());
         getToolbarManager().setShowTitle(mIntentDataProvider.getTitleVisibilityState()
