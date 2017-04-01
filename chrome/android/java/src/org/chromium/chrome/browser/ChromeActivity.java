16475b5f2ca150cfae365e851a725730b14a91bd
Serg <serg.zhukovsky@gmail.com>
2017-04-01 19:46:30 -0400

    Sync functionality(not finished)

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1562,6 +1562,12 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             mContextualSuggestionsCoordinator = new ContextualSuggestionsCoordinator(
                     this, mBottomSheetController, getTabModelSelector());
         }
+
+        // Starting Brave Sync
+        /*ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
+        if (null != app) {
+            app.mBraveSyncWorker = new BraveSyncWorker(this);
+        }*/
     }
 
     /**
