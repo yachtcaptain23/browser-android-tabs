bd62b5a549f191620646ff2b70c849101f3155f1
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-04-17 09:44:09 -0400

    Sync(not finished)

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1246,10 +1246,10 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
         super.finishNativeInitialization();
 
         // Starting Brave Sync
-        /*ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
+        ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
         if (null != app) {
             app.mBraveSyncWorker = new BraveSyncWorker(this);
-        }*/
+        }
     }
 
     /**
