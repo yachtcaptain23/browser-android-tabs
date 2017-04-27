2817eeab900dab7c73cfade2f6c05e39a0e79ced
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-04-27 09:59:46 -0400

    Made it buildable after merge

--- third_party/blink/renderer/core/exported/local_frame_client_impl.cc
+++ third_party/blink/renderer/core/exported/local_frame_client_impl.cc
@@ -314,7 +314,7 @@ bool LocalFrameClientImpl::AllowFingerprinting()
 
 void LocalFrameClientImpl::DeniedFingerprinting()
 {
-  GetContentSettingsClient->DeniedFingerprinting();
+  GetContentSettingsClient()->DeniedFingerprinting();
 }
 
 bool LocalFrameClientImpl::InShadowTree() const {
