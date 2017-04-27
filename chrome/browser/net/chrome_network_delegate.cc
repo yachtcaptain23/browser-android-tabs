df3016574487827d660d643c0223834336354090
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-04-27 09:59:46 -0400

    Made it buildable after merge

--- chrome/browser/net/chrome_network_delegate.cc
+++ chrome/browser/net/chrome_network_delegate.cc
@@ -287,6 +287,8 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
             }
         }
       }
+  } else if (nullptr == shieldsConfig){
+      isGlobalBlockEnabled = true;
   }
   bool isValidUrl = true;
   if (request) {
