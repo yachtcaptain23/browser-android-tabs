66e613c1dd00ce8ace3cb45f515bd0afa8867db9
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-04-27 09:59:46 -0400

    Made it buildable after merge

--- components/ntp_tiles/most_visited_sites.cc
+++ components/ntp_tiles/most_visited_sites.cc
@@ -820,6 +820,7 @@ void MostVisitedSites::OnPopularSitesDownloaded(bool success) {
                                            base::Closure());
     }
   }
+  BuildCurrentTiles();
 }
 
 void MostVisitedSites::OnIconMadeAvailable(const GURL& site_url) {
