7a2c30a0f9d0fa631238fffe225b8f9bfede549d
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-04-27 13:25:30 -0400

    Check on adblock is ready

--- chrome/browser/net/chrome_network_delegate.cc
+++ chrome/browser/net/chrome_network_delegate.cc
@@ -289,7 +289,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
         }
       }
   } else if (nullptr == shieldsConfig){
-      isGlobalBlockEnabled = true;
+      isGlobalBlockEnabled = false;
   }
   bool isValidUrl = true;
   if (request) {
