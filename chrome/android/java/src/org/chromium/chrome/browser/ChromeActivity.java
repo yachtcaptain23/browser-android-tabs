39bcc67cf9ef4a4bb0ba0471e92fd7498ac4527a
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-05-01 09:37:42 -0400

    Updated release notes

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1394,10 +1394,10 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
         super.finishNativeInitialization();
 
         // Starting Brave Sync
-        ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
+        /*ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
         if (null != app) {
             app.mBraveSyncWorker = new BraveSyncWorker(this);
-        }
+        }*/
     }
 
     /**
