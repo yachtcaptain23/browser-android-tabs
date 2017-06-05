23f9a432af7bdb4de7a746b361fb1f41464e84e3
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-06-05 14:57:01 -0400

    Add stats frequency

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -63,6 +63,7 @@ import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.BraveSyncWorker;
+import org.chromium.chrome.browser.StatsUpdaterWorker;
 import org.chromium.chrome.browser.IntentHandler.IntentHandlerDelegate;
 import org.chromium.chrome.browser.IntentHandler.TabOpenType;
 import org.chromium.chrome.browser.appmenu.AppMenu;
@@ -1500,6 +1501,10 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             app.mBraveSyncWorker.Stop();
             app.mBraveSyncWorker = null;
         }
+        if (null != app && null != app.mStatsUpdaterWorker) {
+            app.mStatsUpdaterWorker.Stop();
+            app.mStatsUpdaterWorker = null;
+        }
 
 
         if (mTabContentManager != null) {
@@ -1641,6 +1646,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
         if (null != app) {
             app.mBraveSyncWorker = new BraveSyncWorker(this);
+            app.mStatsUpdaterWorker = new StatsUpdaterWorker(this);
         }
     }
 
--- chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
@@ -38,6 +38,7 @@ import org.chromium.chrome.browser.dependency_injection.ChromeAppModule;
 import org.chromium.chrome.browser.dependency_injection.DaggerChromeAppComponent;
 import org.chromium.chrome.browser.dependency_injection.ModuleFactoryOverrides;
 import org.chromium.chrome.browser.BraveSyncWorker;
+import org.chromium.chrome.browser.StatsUpdaterWorker;
 import org.chromium.chrome.browser.init.InvalidStartupDialog;
 import org.chromium.chrome.browser.instantapps.InstantAppsHandler;
 import org.chromium.chrome.browser.init.ShieldsConfig;
@@ -61,6 +62,8 @@ public class ChromeApplication extends Application {
     private ShieldsConfig mShieldsConfig;
     // Sync worker
     public BraveSyncWorker mBraveSyncWorker = null;
+    // Stats updater
+    public StatsUpdaterWorker mStatsUpdaterWorker = null;
 
     @Nullable
     private static ChromeAppComponent sComponent;
