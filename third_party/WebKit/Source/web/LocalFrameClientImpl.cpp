682ffeedf0495b0e81c98fceaa9c6cbe250395d4
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-06-08 17:13:32 -0400

    Make it buildable after merge

--- third_party/WebKit/Source/web/LocalFrameClientImpl.cpp
+++ third_party/WebKit/Source/web/LocalFrameClientImpl.cpp
@@ -268,7 +268,7 @@ bool LocalFrameClientImpl::HasWebView() const {
 
 void LocalFrameClientImpl::DeniedScript()
 {
-    GetContentSettingsClient()->DeniedScript();
+    GetContentSettingsClient().DeniedScript();
 }
 
 bool LocalFrameClientImpl::InShadowTree() const {
@@ -277,12 +277,12 @@ bool LocalFrameClientImpl::InShadowTree() const {
 
 bool LocalFrameClientImpl::AllowFingerprinting()
 {
-    return GetContentSettingsClient()->allowFingerprinting();
+    return GetContentSettingsClient().AllowFingerprinting();
 }
 
 void LocalFrameClientImpl::DeniedFingerprinting()
 {
-    GetContentSettingsClient()->DeniedFingerprinting();
+    GetContentSettingsClient().DeniedFingerprinting();
 }
 
 Frame* LocalFrameClientImpl::Opener() const {
