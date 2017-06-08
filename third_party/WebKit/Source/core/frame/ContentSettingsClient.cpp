d6989ca8f134e09cd97151080d0b8bbd3e08cf2e
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-06-08 17:13:32 -0400

    Make it buildable after merge

--- third_party/WebKit/Source/core/frame/ContentSettingsClient.cpp
+++ third_party/WebKit/Source/core/frame/ContentSettingsClient.cpp
@@ -128,7 +128,9 @@ void ContentSettingsClient::DeniedScript() {
 
 bool ContentSettingsClient::AllowFingerprinting() {
   if (client_)
-    client_->AllowFingerprinting();
+    return client_->AllowFingerprinting();
+
+  return true;
 }
 
 void ContentSettingsClient::DeniedFingerprinting() {
