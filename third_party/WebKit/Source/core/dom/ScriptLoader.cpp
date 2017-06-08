f95ffccbaf6d2f69b9ee2ecd53c425573ebc4b61
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-06-08 17:13:32 -0400

    Make it buildable after merge

--- third_party/WebKit/Source/core/dom/ScriptLoader.cpp
+++ third_party/WebKit/Source/core/dom/ScriptLoader.cpp
@@ -305,9 +305,9 @@ bool ScriptLoader::PrepareScript(const TextPosition& script_start_position,
   //      steps at this point. The script is not executed."
   if (!context_document->CanExecuteScripts(kAboutToExecuteScript)) {
     if (0 != ScriptContent().length()) {
-        LocalFrame* frame = element_->document().frame();
+        LocalFrame* frame = context_document->GetFrame();;
         if (frame) {
-            frame->loader().client()->DeniedScript();
+            frame->Loader().Client()->DeniedScript();
         }
     }
 
--- third_party/WebKit/Source/core/frame/ContentSettingsClient.cpp
+++ third_party/WebKit/Source/core/frame/ContentSettingsClient.cpp
@@ -134,7 +134,9 @@ void ContentSettingsClient::DeniedScript() {
 
 bool ContentSettingsClient::AllowFingerprinting() {
   if (client_)
-    client_->AllowFingerprinting();
+    return client_->AllowFingerprinting();
+
+  return true;
 }
 
 void ContentSettingsClient::DeniedFingerprinting() {
