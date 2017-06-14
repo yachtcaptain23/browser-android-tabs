c9341084e569a8d3380431ceca153369bd76bc25
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-06-14 09:35:35 -0400

    Crash fix related to permissions

--- components/sync/android/java/src/org/chromium/components/sync/SystemSyncContentResolverDelegate.java
+++ components/sync/android/java/src/org/chromium/components/sync/SystemSyncContentResolverDelegate.java
@@ -20,41 +20,63 @@ public class SystemSyncContentResolverDelegate implements SyncContentResolverDel
 
     @Override
     public void removeStatusChangeListener(Object handle) {
-        ContentResolver.removeStatusChangeListener(handle);
+        try {
+            ContentResolver.removeStatusChangeListener(handle);
+        } catch (SecurityException exc) {}
     }
 
     @Override
     public void setMasterSyncAutomatically(boolean sync) {
-        ContentResolver.setMasterSyncAutomatically(sync);
+        try {
+            ContentResolver.setMasterSyncAutomatically(sync);
+        } catch (SecurityException exc) {}
     }
 
     @Override
     public boolean getMasterSyncAutomatically() {
-        return ContentResolver.getMasterSyncAutomatically();
+        try {
+            return ContentResolver.getMasterSyncAutomatically();
+        } catch (SecurityException exc) {}
+
+        return false;
     }
 
     @Override
     public boolean getSyncAutomatically(Account account, String authority) {
-        return ContentResolver.getSyncAutomatically(account, authority);
+        try {
+            return ContentResolver.getSyncAutomatically(account, authority);
+        } catch (SecurityException exc) {}
+
+        return false;
     }
 
     @Override
     public void setSyncAutomatically(Account account, String authority, boolean sync) {
-        ContentResolver.setSyncAutomatically(account, authority, sync);
+        try {
+            ContentResolver.setSyncAutomatically(account, authority, sync);
+        } catch (SecurityException exc) {}
     }
 
     @Override
     public void setIsSyncable(Account account, String authority, int syncable) {
-        ContentResolver.setIsSyncable(account, authority, syncable);
+        try {
+            ContentResolver.setIsSyncable(account, authority, syncable);
+        } catch (SecurityException exc) {}
     }
 
     @Override
     public int getIsSyncable(Account account, String authority) {
-        return ContentResolver.getIsSyncable(account, authority);
+        try {
+            return ContentResolver.getIsSyncable(account, authority);
+        } catch (SecurityException exc) {}
+
+        return 0;
     }
 
     @Override
     public void removePeriodicSync(Account account, String authority, Bundle extras) {
-        ContentResolver.removePeriodicSync(account, authority, extras);
+        try {
+            ContentResolver.removePeriodicSync(account, authority, extras);
+        } catch (SecurityException exc) {}
     }
 }
