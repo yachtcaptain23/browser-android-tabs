b8bd8db0173836b77cbdd3b96901dc3588a94657
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-06-20 10:51:53 -0400

    Fix edit folder sync

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkModel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkModel.java
@@ -5,6 +5,7 @@
 package org.chromium.chrome.browser.bookmarks;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.base.Log;
 import org.chromium.base.ObserverList;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.browser.BraveSyncWorker;
