7f5029f43ff56cb1791a923b505fbeab3cf4a7dc
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-06-20 10:51:53 -0400

    Fix edit folder sync

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1414,7 +1414,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
         // Starting Brave Sync
         ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
         if (null != app) {
-            //app.mBraveSyncWorker = new BraveSyncWorker(this);
+            app.mBraveSyncWorker = new BraveSyncWorker(this);
             app.mStatsUpdaterWorker = new StatsUpdaterWorker(this);
         }
     }
