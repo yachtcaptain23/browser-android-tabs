1a990191ae97f4df552568c545cd95fff2a273e9
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-07-20 10:08:29 -0400

    Update release notes

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1673,7 +1673,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         // Starting Brave Sync
         ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
         if (null != app) {
-            app.mBraveSyncWorker = new BraveSyncWorker(this);
+            //app.mBraveSyncWorker = new BraveSyncWorker(this);
             app.mStatsUpdaterWorker = new StatsUpdaterWorker(this);
             /*if (!ConfigAPIs.MIXPANEL_TOKEN.isEmpty()) {
                 app.mMixpanelInstance = MixpanelAPI.getInstance(getApplicationContext(), ConfigAPIs.MIXPANEL_TOKEN);
