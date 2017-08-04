f05485566931d73a0b9b442b519e50e8bc36d33f
samartnik <artem@brave.com>
2017-08-04 14:41:46 +0300

    New Tab Page with Brave stats

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPage.java
@@ -429,6 +429,7 @@ public class NewTabPage
 
     private void updateSearchProviderHasLogo() {
         mSearchProviderHasLogo = TemplateUrlService.getInstance().doesDefaultSearchEngineHaveLogo();
+        mSearchProviderHasLogo = false;//TemplateUrlService.getInstance().doesDefaultSearchEngineHaveLogo();
     }
 
     private void onSearchEngineUpdated() {
--- chrome/browser/net/chrome_network_delegate.cc
+++ chrome/browser/net/chrome_network_delegate.cc
@@ -305,7 +305,8 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
   if (enable_tracking_protection_ && !shieldsSetExplicitly) {
     isTPEnabled = enable_tracking_protection_->GetValue();
   }
-  int adsAndTrackersBlocked = 0;
+  int adsBlocked = 0;
+  int trackersBlocked = 0;
   int httpsUpgrades = 0;
 	if (request
       && !firstPartyUrl
@@ -318,7 +319,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
 					request->url().host())
 				) {
 		block = true;
-    adsAndTrackersBlocked++;
+    trackersBlocked++;
 	}
   bool isAdBlockEnabled = true;
   if (enable_ad_block_ && !shieldsSetExplicitly) {
@@ -344,7 +345,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
 					(unsigned int)info->GetResourceType(),
           isAdBlockRegionalEnabled)) {
 		block = true;
-    adsAndTrackersBlocked++;
+    adsBlocked++;
 	}
   bool check_httpse_redirect = true;
   if (block && info && content::RESOURCE_TYPE_IMAGE == info->GetResourceType()) {
@@ -370,9 +371,10 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
     }
   }
   //
-  if (nullptr != shieldsConfig && (0 != adsAndTrackersBlocked || 0 != httpsUpgrades)) {
+  if (nullptr != shieldsConfig && (0 != trackersBlocked || 0 != adsBlocked || 0 != httpsUpgrades)) {
     shieldsConfig->setBlockedCountInfo(last_first_party_url_.spec()
-        , adsAndTrackersBlocked
+        , trackersBlocked
+        , adsBlocked
         , httpsUpgrades
         , 0
         , 0);
