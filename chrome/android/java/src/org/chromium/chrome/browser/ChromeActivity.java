06dd4ae6cdace06f71145ec73b56f1da99a258ff
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-08-04 15:36:39 -0400

    Make it buildable after merge

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -2700,6 +2700,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
     @Deprecated
     public DiscardableReferencePool getReferencePool() {
         return mReferencePool;
+    }
 
     /**
      * Updates Bravery Panel counts
