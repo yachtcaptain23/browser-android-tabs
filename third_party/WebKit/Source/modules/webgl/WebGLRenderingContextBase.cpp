2f923f68e6f056e6b938e374aa57d1c49ef78d6d
samartnik <artem@brave.com>
2017-08-09 11:38:59 +0300

    Added WebRTC blocking for fingerprinting protection

--- third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.cpp
@@ -3361,7 +3361,7 @@ Nullable<Vector<String>> WebGLRenderingContextBase::getSupportedExtensions() {
     if (frame) {
         allowed = frame->Loader().Client()->AllowFingerprinting();
         if (!allowed && !canvas()->wasBlockedByFingerprinting()) {
-            frame->Loader().Client()->DeniedFingerprinting();;
+            frame->Loader().Client()->DeniedFingerprinting();
         }
     }
     if (!allowed)
