47b8f73a557a031a5ec82751536bb0b500cf2959
samartnik <artem@brave.com>
2017-08-18 22:56:28 +0300

    Initial 60.0.3112.107

---
+++ third_party/WebKit/LayoutTests/usb/resources/usb-helpers.js
@@ -0,0 +1,95 @@
+'use strict';
+
+function usb_test(func, name, properties) {
+  promise_test(async () => {
+    await navigator.usb.test.initialize()
+    try {
+      await func();
+    } finally {
+      await navigator.usb.test.reset();
+    }
+  }, name, properties);
+}
+
+// Returns a promise that is resolved when the next USBConnectionEvent of the
+// given type is received.
+function connectionEventPromise(eventType) {
+  return new Promise(resolve => {
+    let eventHandler = e => {
+      assert_true(e instanceof USBConnectionEvent);
+      navigator.usb.removeEventListener(eventType, eventHandler);
+      resolve(e.device);
+    };
+    navigator.usb.addEventListener(eventType, eventHandler);
+  });
+}
+
+// Creates a fake device and returns a promise that resolves once the
+// 'connect' event is fired for the fake device. The promise is resolved with
+// an object containing the fake USB device and the corresponding USBDevice.
+function getFakeDevice() {
+  let promise = connectionEventPromise('connect');
+  let fakeDevice = navigator.usb.test.addFakeDevice(fakeDeviceInit);
+  return promise.then(device => {
+    return { device: device, fakeDevice: fakeDevice };
+  });
+}
+
+// Disconnects the given device and returns a promise that is resolved when it
+// is done.
+function waitForDisconnect(fakeDevice) {
+  let promise = connectionEventPromise('disconnect');
+  fakeDevice.disconnect();
+  return promise;
+}
+
+function assertRejectsWithError(promise, name, message) {
+  return promise.then(() => {
+    assert_unreached('expected promise to reject with ' + name);
+  }, error => {
+    assert_equals(error.name, name);
+    if (message !== undefined)
+      assert_equals(error.message, message);
+  });
+}
+
+function assertDeviceInfoEquals(usbDevice, deviceInit) {
+  for (var property in deviceInit) {
+    if (property == 'activeConfigurationValue') {
+      if (deviceInit.activeConfigurationValue == 0) {
+        assert_equals(usbDevice.configuration, null);
+      } else {
+        assert_equals(usbDevice.configuration.configurationValue,
+                      deviceInit.activeConfigurationValue);
+      }
+    } else if (Array.isArray(deviceInit[property])) {
+      assert_equals(usbDevice[property].length, deviceInit[property].length);
+      for (var i = 0; i < usbDevice[property].length; ++i)
+        assertDeviceInfoEquals(usbDevice[property][i], deviceInit[property][i]);
+    } else {
+      assert_equals(usbDevice[property], deviceInit[property], property);
+    }
+  }
+}
+
+// TODO(reillyg): Remove when jyasskin upstreams this to testharness.js:
+// https://crbug.com/509058.
+function callWithKeyDown(functionCalledOnKeyPress) {
+  return new Promise(resolve => {
+    function onKeyPress() {
+      document.removeEventListener('keypress', onKeyPress, false);
+      resolve(functionCalledOnKeyPress());
+    }
+    document.addEventListener('keypress', onKeyPress, false);
+
+    eventSender.keyDown(' ', []);
+  });
+}
+
+function runGarbageCollection() {
+  // Run gc() as a promise.
+  return new Promise((resolve, reject) => {
+    GCController.collect();
+    setTimeout(resolve, 0);
+  });
+}
---
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/common.py
@@ -0,0 +1,83 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Utility functions used both when importing and exporting."""
+
+import logging
+
+from webkitpy.w3c.chromium_commit import ChromiumCommit
+from webkitpy.w3c.chromium_finder import absolute_chromium_dir
+
+
+WPT_DEST_NAME = 'wpt'
+WPT_GH_ORG = 'w3c'
+WPT_GH_REPO_NAME = 'web-platform-tests'
+WPT_GH_URL = 'https://github.com/%s/%s/' % (WPT_GH_ORG, WPT_GH_REPO_NAME)
+WPT_GH_SSH_URL_TEMPLATE = 'https://{}@github.com/%s/%s.git' % (WPT_GH_ORG, WPT_GH_REPO_NAME)
+WPT_REVISION_FOOTER = 'WPT-Export-Revision:'
+
+# TODO(qyearsley): This directory should be able to be constructed with
+# PathFinder and WPT_DEST_NAME, plus the string "external".
+CHROMIUM_WPT_DIR = 'third_party/WebKit/LayoutTests/external/wpt/'
+
+# Our mirrors of the official wpt repo, which we pull from.
+WPT_REPO_URL = 'https://chromium.googlesource.com/external/w3c/web-platform-tests.git'
+
+
+_log = logging.getLogger(__name__)
+
+
+def exportable_commits_since(chromium_commit_hash, host, local_wpt):
+    """Lists exportable commits after a certain point.
+
+    Args:
+        chromium_commit_hash: The SHA of the Chromium commit from which this
+            method will look. This commit is not included in the commits searched.
+        host: A Host object.
+        local_wpt: A LocalWPT instance, used to see whether a Chromium commit
+            can be applied cleanly in the upstream repo.
+
+    Returns:
+        A list of ChromiumCommit objects for commits that are exportable after
+        the given commit, in chronological order.
+    """
+    chromium_repo_root = host.executive.run_command([
+        'git', 'rev-parse', '--show-toplevel'
+    ], cwd=absolute_chromium_dir(host)).strip()
+
+    wpt_path = chromium_repo_root + '/' + CHROMIUM_WPT_DIR
+    commit_range = '{}..HEAD'.format(chromium_commit_hash)
+    commit_hashes = host.executive.run_command([
+        'git', 'rev-list', commit_range, '--reverse', '--', wpt_path
+    ], cwd=absolute_chromium_dir(host)).splitlines()
+    chromium_commits = [ChromiumCommit(host, sha=sha) for sha in commit_hashes]
+    return [commit for commit in chromium_commits if is_exportable(commit, local_wpt)]
+
+
+def exportable_commits_over_last_n_commits(number, host, local_wpt):
+    """Lists exportable commits after a certain point.
+
+    Args:
+        number: Number of commits back to look. The commits to check will
+            include all commits starting from the commit before HEAD~n, up
+            to and including HEAD.
+        host: A Host object.
+        local_wpt: A LocalWPT instance, used to see whether a Chromium commit
+            can be applied cleanly in the upstream repo.
+
+    Returns:
+        A list of ChromiumCommit objects for commits that are exportable after
+        the given commit, in chronological order.
+    """
+    return exportable_commits_since('HEAD~{}'.format(number + 1), host, local_wpt)
+
+
+def is_exportable(chromium_commit, local_wpt):
+    """Checks whether a given patch is exportable and can be applied."""
+    patch = chromium_commit.format_patch()
+
+    return ('NOEXPORT=true' not in chromium_commit.message() and
+            not chromium_commit.message().startswith('Import') and
+            patch and
+            local_wpt.test_patch(patch, chromium_commit))
