58f06a64a021096fd7b99c0354a9ff3a026f9f70
samartnik <artem@brave.com>
2017-08-21 18:25:35 +0300

    Updated third parties script

---
+++ scripts/postThirdPartiesSetup.js
@@ -0,0 +1,3 @@
+sh scripts/updateSubModules.js
+sh scripts/copyAdBlockTPFiles.js
+cp -f chrome/android/java/src/org/chromium/chrome/browser/ConfigAPIs.java.template chrome/android/java/src/org/chromium/chrome/browser/ConfigAPIs.java
