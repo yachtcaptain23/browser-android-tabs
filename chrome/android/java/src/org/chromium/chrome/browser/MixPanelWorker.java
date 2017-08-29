3ff27fa80f42d7942a112aced82ad6dae78fbbbc
samartnik <artem@brave.com>
2017-08-29 12:05:11 +0300

    Changes for Mixpanel stats

--- chrome/android/java/src/org/chromium/chrome/browser/MixPanelWorker.java
+++ chrome/android/java/src/org/chromium/chrome/browser/MixPanelWorker.java
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
- 
+
 package org.chromium.chrome.browser;
 
 import org.chromium.base.ContextUtils;
@@ -10,7 +10,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MixPanelWorker {
-    public static void SendEvent(String eventName, String propertyName, String propertyValue) {
+    // Send event with no options
+    public static void SendEvent(String eventName) {
+        ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
+        if (null != app && null != app.mMixpanelInstance) {
+            app.mMixpanelInstance.track(eventName);
+          }
+    }
+
+    // Send event with option
+    public static void SendEvent(String eventName, String propertyName, Object propertyValue) {
         ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
         if (null != app && null != app.mMixpanelInstance) {
             try {
@@ -20,5 +29,22 @@ public class MixPanelWorker {
             } catch (JSONException e) {
             }
         }
+    }
+
+    // Send event for Brave app start
+    public static void SendBraveAppStartEvent(boolean httpsEverywhere, boolean trackingProtection, boolean adBlock, boolean regionalAdBlock, boolean fingerprintingProtection) {
+      ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
+      if (null != app && null != app.mMixpanelInstance) {
+          try {
+              JSONObject obj = new JSONObject();
+              obj.put("HTTPS Everywhere", httpsEverywhere);
+              obj.put("Tracking Protection Mode", trackingProtection);
+              obj.put("Ad Block", adBlock);
+              obj.put("Regional Ad Block", regionalAdBlock);
+              obj.put("Fingerprinting Protection", fingerprintingProtection);
+              app.mMixpanelInstance.track("Brave App Start", obj);
+          } catch (JSONException e) {
+          }
+      }
     }
 }
