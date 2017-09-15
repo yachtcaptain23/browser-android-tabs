38112f82de731b9fac19ca96054e9a6be93bacea
Alexey <alexey.barabash@gmail.com>
2017-09-15 16:32:53 +0300

    Desktop mode settings after re-base 1

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -2485,6 +2485,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             final boolean reloadOnChange = !currentTab.isNativePage();
             final boolean usingDesktopUserAgent =
                     currentTab.getWebContents().getNavigationController().getUseDesktopUserAgent();
+            currentTab.setDesktopModeOverridenByTab(true);
             currentTab.getWebContents().getNavigationController().setUseDesktopUserAgent(
                     !usingDesktopUserAgent, reloadOnChange);
             RecordUserAction.record("MobileMenuRequestDesktopSite");
