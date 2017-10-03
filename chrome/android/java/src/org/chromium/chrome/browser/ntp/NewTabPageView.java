13f5b5898123430ad3d7d9d37f65cecef6bfb36c
samartnik <artem@brave.com>
2017-10-03 16:08:11 +0300

    Changed the way we update Brave stats

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -388,7 +388,6 @@ public class NewTabPageView extends FrameLayout implements TileGroup.Observer, O
             }
         });*/
         initializeSearchBoxScrollHandling();
-        initializeBraveStats();
         mInitialized = true;
 
         TraceEvent.end(TAG + ".initialize()");
@@ -513,8 +512,8 @@ public class NewTabPageView extends FrameLayout implements TileGroup.Observer, O
     /**
      * Sets up Brave stats.
      */
-    private void initializeBraveStats() {
-        TraceEvent.begin(TAG + ".initializeBraveStats()");
+    private void updateBraveStats() {
+        TraceEvent.begin(TAG + ".updateBraveStats()");
         long trackersBlockedCount = mSharedPreferences.getLong(PREF_TRACKERS_BLOCKED_COUNT, 0);
         long adsBlockedCount = mSharedPreferences.getLong(PREF_ADS_BLOCKED_COUNT, 0);
         long httpsUpgradesCount = mSharedPreferences.getLong(PREF_HTTPS_UPGRADES_COUNT, 0);
@@ -527,7 +526,7 @@ public class NewTabPageView extends FrameLayout implements TileGroup.Observer, O
         adsBlockedCountTextView.setText(getBraveStatsStringFormNumber(adsBlockedCount));
         httpsUpgradesCountTextView.setText(getBraveStatsStringFormNumber(httpsUpgradesCount));
         estTimeSavedTextView.setText(getBraveStatsStringFromTime(estimatedMillisecondsSaved / 1000));
-        TraceEvent.end(TAG + ".initializeBraveStats()");
+        TraceEvent.end(TAG + ".updateBraveStats()");
     }
 
     /*
@@ -1019,6 +1018,8 @@ public class NewTabPageView extends FrameLayout implements TileGroup.Observer, O
         // starts the FRE in one orientation, changes an orientation and then leaves the FRE the
         // UiConfig will have the wrong orientation. https://crbug.com/683886.
         mUiConfig.updateDisplayStyle();
+
+        updateBraveStats();
 
         if (visibility == VISIBLE) {
             updateVoiceSearchButtonVisibility();
