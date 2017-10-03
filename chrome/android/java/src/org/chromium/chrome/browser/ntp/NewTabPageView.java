5f553abcbf294524793107aa4d2471ec89150b58
samartnik <artem@brave.com>
2017-10-03 16:08:11 +0300

    Changed the way we update Brave stats

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -223,7 +223,6 @@ public class NewTabPageView extends FrameLayout {
         });
 
         manager.addDestructionObserver(NewTabPageView.this::onDestroy);
-        initializeBraveStats();
 
         TraceEvent.end(TAG + ".initialize()");
     }
@@ -258,8 +257,8 @@ public class NewTabPageView extends FrameLayout {
     /**
      * Sets up Brave stats.
      */
-    private void initializeBraveStats() {
-        TraceEvent.begin(TAG + ".initializeBraveStats()");
+    private void updateBraveStats() {
+        TraceEvent.begin(TAG + ".updateBraveStats()");
         long trackersBlockedCount = mSharedPreferences.getLong(PREF_TRACKERS_BLOCKED_COUNT, 0);
         long adsBlockedCount = mSharedPreferences.getLong(PREF_ADS_BLOCKED_COUNT, 0);
         long httpsUpgradesCount = mSharedPreferences.getLong(PREF_HTTPS_UPGRADES_COUNT, 0);
@@ -272,7 +271,7 @@ public class NewTabPageView extends FrameLayout {
         adsBlockedCountTextView.setText(getBraveStatsStringFormNumber(adsBlockedCount));
         httpsUpgradesCountTextView.setText(getBraveStatsStringFormNumber(httpsUpgradesCount));
         estTimeSavedTextView.setText(getBraveStatsStringFromTime(estimatedMillisecondsSaved / 1000));
-        TraceEvent.end(TAG + ".initializeBraveStats()");
+        TraceEvent.end(TAG + ".updateBraveStats()");
     }
 
     /*
@@ -365,6 +364,12 @@ public class NewTabPageView extends FrameLayout {
         // immediately attached to the window if the RecyclerView is scrolled when the NTP
         // is refocused.
         if (mManager.isLocationBarShownInNTP()) mNewTabPageLayout.updateSearchBoxOnScroll();
+    }
+
+    @Override
+    protected void onWindowVisibilityChanged(int visibility) {
+        super.onWindowVisibilityChanged(visibility);
+        updateBraveStats();
     }
 
     /**
