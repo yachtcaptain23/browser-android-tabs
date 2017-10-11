662e9f4a07e3faa90a5eb5e16ea4dd742a57c135
AlexeyBarabash <alexey@brave.com>
2017-10-11 14:32:28 +0300

    modified names

--- chrome/android/java/src/org/chromium/chrome/browser/init/InstallationSourceInformer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/InstallationSourceInformer.java
@@ -16,19 +16,19 @@ public class InstallationSourceInformer {
   private static final String PREF_MIXPANEL_INSTALL_SOURCE_INFORMED = "mixpanel_installation_source_informed";
 
   public static void InformFromOther() {
-    //Disabled until we not ensured InformFromAdWords works well
+    //Disabled until we not ensured InformFromPromo works well
     //Inform("Others");
     Log.i("TAG", "InstallationSourceInformer.InformFromOther skip send info");
   }
 
   public static void InformFromPlayMarket() {
-    //Disabled until we not ensured InformFromAdWords works well
+    //Disabled until we not ensured InformFromPromo works well
     //Inform("Google Play");
     Log.i("TAG", "InstallationSourceInformer.InformFromPlayMarket skip send info");
   }
 
-  public static void InformFromAdWords() {
-    Inform("AdWords");
+  public static void InformFromPromo() {
+    Inform("Promo");
   }
 
   private static synchronized void Inform(String sourceName) {
