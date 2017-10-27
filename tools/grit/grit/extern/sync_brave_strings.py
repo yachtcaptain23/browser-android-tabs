b4862b559eefd672bfdfc3b3feda88e03cc9fbb4
samartnik <artem@brave.com>
2017-10-27 16:07:52 +0300

    Added list of not used strings

--- tools/grit/grit/extern/sync_brave_strings.py
+++ tools/grit/grit/extern/sync_brave_strings.py
@@ -1,21 +1,26 @@
 import sys
 import xml.etree.ElementTree
 
-# these are hard coded values
+# These are hard coded values
 base_strings_file='../../../../chrome/android/java/strings/strings.xml'
 chrome_strings_file='../../../../chrome/android/java/strings/android_chrome_strings.grd'
+# Strings that are not used at the moment
+skip_string_names={'brave_settings_title', 'brave_shields_down', 'brave_shields_up', 'brave_shields_ads_trackers',
+'brave_shields_block_3rd_party_cookies_switch', 'brave_shields_fingerprints_blocked'}
 
-# go through the strings in the base file and check for appropriate string in chrome strings file
+# Go through the strings in the base file and check for appropriate string in chrome strings file
 def CheckStrings():
     base_strings_file_modified = False
     strings_tree = xml.etree.ElementTree.parse(base_strings_file)
     strings = strings_tree.getroot()
     for string_tag in strings.findall('string'):
         string_name = string_tag.get('name')
+        if string_name in skip_string_names:
+            continue
         string_value = string_tag.text.strip()
         if not string_name:
             sys.exit('String name is empty')
-        string_name_ids = 'IDS_' + string_name.upper()    
+        string_name_ids = 'IDS_' + string_name.upper()
         messages = xml.etree.ElementTree.parse(chrome_strings_file).getroot().find('release').find('messages')
         message_found = False
         for message_tag in messages.findall('message'):
@@ -27,7 +32,7 @@ def CheckStrings():
                 if not string_value == message_value:
                     string_tag.text = message_value
                     base_strings_file_modified = True
-                    print('String "' + string_name + '" updated with value "' + message_value + '"')                
+                    print('String "' + string_name + '" updated with value "' + message_value + '"')
                 message_found = True
                 break
         if not message_found:
@@ -36,15 +41,16 @@ def CheckStrings():
         strings_tree.write(base_strings_file)
     print('Strings check finished successfully')
 
-# check for brave strings in grd file and update or add them (depending on their existance in strings.xml)
+# Check for brave strings in grd file and update or add them (depending on their existance in strings.xml)
+# Not implemented yet
 def SyncFromGrdToStrings():
-    messages = xml.etree.ElementTree.parse(chrome_strings_file).getroot().find('release').find('messages')    
+    messages = xml.etree.ElementTree.parse(chrome_strings_file).getroot().find('release').find('messages')
     for message_tag in messages.findall('message'):
         message_name = message_tag.get('name')
         message_value = message_tag.text.strip()
         if not message_name:
             sys.exit('Message name is empty')
 
-# run both functions
-SyncFromGrdToStrings()
+# Run both functions
+# SyncFromGrdToStrings()
 CheckStrings()
