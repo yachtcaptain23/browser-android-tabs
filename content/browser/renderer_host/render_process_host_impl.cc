71cd5aec057e846d04c801fa9be0cc6726b89352
AlexeyBarabash <alexey@brave.com>
2017-11-07 11:02:26 +0200

    play video in background

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -2500,12 +2500,20 @@ void RenderProcessHostImpl::AppendRendererCommandLine(
       base::DoubleToString(display::win::GetDPIScale()));
 #endif
 
+  if (NeedPlayVideoInBackground()) {
+    command_line->AppendSwitch(switches::kDisableMediaSuspend);
+  }
+
   AppendCompositorCommandLineFlags(command_line);
 
   command_line->AppendSwitchASCII(switches::kServiceRequestChannelToken,
                                   child_connection_->service_token());
   command_line->AppendSwitchASCII(switches::kRendererClientId,
                                   std::to_string(GetID()));
+}
+
+bool RenderProcessHostImpl::NeedPlayVideoInBackground() const {
+  return true;
 }
 
 void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
