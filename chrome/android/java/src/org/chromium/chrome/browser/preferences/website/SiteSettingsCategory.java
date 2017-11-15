02af0177248fd7348794926ec530e20ba8d7147f
AlexeyBarabash <alexey@brave.com>
2017-11-15 16:06:45 +0200

    separate settings, fix for mobile youtube

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SiteSettingsCategory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SiteSettingsCategory.java
@@ -70,6 +70,9 @@ public class SiteSettingsCategory {
         int NUM_ENTRIES = 18;
     }
 
+    public static final String CATEGORY_DESKTOP_VIEW = "desktop_view";
+    public static final String CATEGORY_PLAY_VIDEO_IN_BACKGROUND = "play_video_in_background";
+
     // The id of this category.
     private @Type int mCategory;
 
@@ -120,6 +123,9 @@ public class SiteSettingsCategory {
         if (contentSettingsType == ContentSettingsType.CONTENT_SETTINGS_TYPE_DESKTOP_VIEW) {
             return fromString(CATEGORY_DESKTOP_VIEW);
         }
+        if (contentSettingsType == ContentSettingsType.CONTENT_SETTINGS_TYPE_PLAY_VIDEO_IN_BACKGROUND) {
+            return fromString(CATEGORY_PLAY_VIDEO_IN_BACKGROUND);
+        }
         return null;
     }
 
@@ -129,13 +135,6 @@ public class SiteSettingsCategory {
             if (preferenceKey(i).equals(preferenceKey)) return createFromType(i);
         }
         return null;
-    }
-
-    /**
-     * Returns whether this category is the Desktop View category.
-     */
-    public boolean showDesktopViewSites() {
-        return mContentSettingsType == ContentSettingsType.CONTENT_SETTINGS_TYPE_DESKTOP_VIEW;
     }
 
     /**
