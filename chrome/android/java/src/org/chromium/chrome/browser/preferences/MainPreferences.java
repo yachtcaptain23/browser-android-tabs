3086091b89ceda00f7b84684879c1078137ab931
samartnik <artem@brave.com>
2017-12-07 11:59:38 +0200

    Fixed build after rebase

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/MainPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/MainPreferences.java
@@ -77,7 +77,7 @@ public class MainPreferences extends PreferenceFragment
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mSignInPreference.onPreferenceFragmentDestroyed();
+        //mSignInPreference.onPreferenceFragmentDestroyed();
     }
 
     @Override
