d868713de4f33df383a1e9b9a4a60190914c6cd0
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-12-08 12:11:58 -0500

    Fixed DDG UA

--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -1180,7 +1180,11 @@ void FrameLoader::RestoreScrollPositionAndViewState(
 }
 
 String FrameLoader::UserAgent(const std::string& strHost) const {
-  String user_agent = Client()->UserAgent(strHost);
+  std::string newHost = strHost;
+  if (provisional_document_loader_) {
+    newHost = provisional_document_loader_->Url().GetString().Utf8().data();
+  }
+  String user_agent = Client()->UserAgent(newHost);
   probe::applyUserAgentOverride(frame_->GetDocument(), &user_agent);
   return user_agent;
 }
