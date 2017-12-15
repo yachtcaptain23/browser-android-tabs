remzert/BraveBrowser

Added regional adblock
Auditors: @bbondy

chrome/browser/profiles/profile_io_data.cc
@@ -480,6 +480,7 @@ void ProfileIOData::InitializeOnUIThread(Profile* profile) {
       &enable_httpse_,
       &enable_tracking_protection_,
       &enable_ad_block_,
+      &enable_ad_block_regional_,
       &force_google_safesearch_,
       &force_youtube_restrict_,
       &allowed_domains_for_apps_,
@@ -1067,6 +1068,7 @@ void ProfileIOData::Init(
   network_delegate->set_enable_httpse(&enable_httpse_);
   network_delegate->set_enable_tracking_protection(&enable_tracking_protection_);
   network_delegate->set_enable_ad_block(&enable_ad_block_);
+  network_delegate->set_enable_ad_block_regional(&enable_ad_block_regional_);
   network_delegate->set_force_google_safe_search(&force_google_safesearch_);
   network_delegate->set_force_youtube_restrict(&force_youtube_restrict_);
   network_delegate->set_allowed_domains_for_apps(&allowed_domains_for_apps_);
@@ -1288,6 +1290,7 @@ void ProfileIOData::ShutdownOnUIThread(
   enable_httpse_.Destroy();
   enable_tracking_protection_.Destroy();
   enable_ad_block_.Destroy();
+  enable_ad_block_regional_.Destroy();
   force_google_safesearch_.Destroy();
   force_youtube_restrict_.Destroy();
   allowed_domains_for_apps_.Destroy();

yachtcaptain23/browser-android-tabs

Conflicts:
	chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
	chrome/common/pref_names.cc
	chrome/common/pref_names.h

chrome/browser/profiles/profile_io_data.cc
@@ -473,6 +473,7 @@ void ProfileIOData::InitializeOnUIThread(Profile* profile) {
       &enable_httpse_,
       &enable_tracking_protection_,
       &enable_ad_block_,
+      &enable_ad_block_regional_,
       &force_google_safesearch_,
       &force_youtube_restrict_,
       &allowed_domains_for_apps_,
@@ -1050,6 +1051,7 @@ void ProfileIOData::Init(
   chrome_network_delegate->set_enable_httpse(&enable_httpse_);
   chrome_network_delegate->set_enable_tracking_protection(&enable_tracking_protection_);
   chrome_network_delegate->set_enable_ad_block(&enable_ad_block_);
+  chrome_network_delegate->set_enable_ad_block_regional(&enable_ad_block_regional_);
   chrome_network_delegate->set_force_google_safe_search(
       &force_google_safesearch_);
   chrome_network_delegate->set_force_youtube_restrict(&force_youtube_restrict_);
@@ -1381,6 +1383,7 @@ void ProfileIOData::ShutdownOnUIThread(
   enable_httpse_.Destroy();
   enable_tracking_protection_.Destroy();
   enable_ad_block_.Destroy();
+  enable_ad_block_regional_.Destroy();
   force_google_safesearch_.Destroy();
   force_youtube_restrict_.Destroy();
   allowed_domains_for_apps_.Destroy();