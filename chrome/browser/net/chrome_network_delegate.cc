1b88b4ab114cf106a4c8d36f3e230ec308f95695
samartnik <artem@brave.com>
2018-01-26 12:59:16 +0200

    Fixed build after rebasing

--- chrome/browser/net/chrome_network_delegate.cc
+++ chrome/browser/net/chrome_network_delegate.cc
@@ -498,7 +498,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest_TpBlockPreFileWork(
 }
 
 void ChromeNetworkDelegate::OnBeforeURLRequest_TpBlockFileWork() {
-  base::ThreadRestrictions::AssertIOAllowed();
+  base::AssertBlockingAllowed();
   DCHECK_CURRENTLY_ON(content::BrowserThread::FILE);
   blockers_worker_->InitTP();
 }
@@ -575,7 +575,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest_AdBlockPreFileWork(
 }
 
 void ChromeNetworkDelegate::OnBeforeURLRequest_AdBlockFileWork(std::shared_ptr<OnBeforeURLRequestContext> ctx) {
-  base::ThreadRestrictions::AssertIOAllowed();
+  base::AssertBlockingAllowed();
   DCHECK_CURRENTLY_ON(content::BrowserThread::FILE);
   blockers_worker_->InitAdBlock();
 
@@ -659,7 +659,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest_HttpsePreFileWork(
 
 void ChromeNetworkDelegate::OnBeforeURLRequest_HttpseFileWork(net::URLRequest* request, std::shared_ptr<OnBeforeURLRequestContext> ctx)
 {
-  base::ThreadRestrictions::AssertIOAllowed();
+  base::AssertBlockingAllowed();
   DCHECK_CURRENTLY_ON(content::BrowserThread::FILE);
   DCHECK(ctx->request_identifier != 0);
   ctx->newURL = blockers_worker_->getHTTPSURL(&ctx->UrlCopy, ctx->request_identifier);
