518ce6bdaeae1926db66e85b274227a4a6c32bde
samartnik <artem@brave.com>
2018-01-31 09:35:37 +0200

    Disabled sending metrics

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
@@ -59,6 +59,7 @@ public class PrivacyPreferences extends PreferenceFragment
             "sync_and_services_link_divider";
     private static final String PREF_SYNC_AND_SERVICES_LINK = "sync_and_services_link";
     private static final String PREF_USAGE_STATS = "usage_stats_reporting";
+    //private static final String PREF_SEND_METRICS = "send_metrics";
 
     private ManagedPreferenceDelegate mManagedPreferenceDelegate;
 
@@ -173,10 +174,10 @@ public class PrivacyPreferences extends PreferenceFragment
         adBlockRegionalPref.setOnPreferenceChangeListener(this);
         adBlockRegionalPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
 
-        ChromeBaseCheckBoxPreference sendMetricsPref =
+        /*ChromeBaseCheckBoxPreference sendMetricsPref =
                 (ChromeBaseCheckBoxPreference) findPreference(PREF_SEND_METRICS);
         sendMetricsPref.setOnPreferenceChangeListener(this);
-        sendMetricsPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
+        sendMetricsPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);*/
 
         updateSummaries();
     }
@@ -214,7 +215,11 @@ public class PrivacyPreferences extends PreferenceFragment
         } else if (PREF_CAN_MAKE_PAYMENT.equals(key)) {
             PrefServiceBridge.getInstance().setBoolean(
                     Pref.CAN_MAKE_PAYMENT_ENABLED, (boolean) newValue);
-        }
+        }/* else if (PREF_SEND_METRICS.equals(key)) {
+            SharedPreferences.Editor sharedPreferencesEditor = ContextUtils.getAppSharedPreferences().edit();
+            sharedPreferencesEditor.putBoolean(PREF_SEND_METRICS, (boolean)newValue);
+            sharedPreferencesEditor.apply();
+        }*/
 
         return true;
     }
