remzert/BraveBrowser

Made it buildable

third_party/WebKit/Source/core/html/HTMLCanvasElement.h
@@ -237,6 +237,8 @@ class CORE_EXPORT HTMLCanvasElement final : public HTMLElement,
 
   void detachContext() { m_context = nullptr; }
 
+  bool wasBlockedByFingerprinting() { return m_wasBlockedByFingerprinting; }
+
  protected:
   void didMoveToNewDocument(Document& oldDocument) override;
 

yachtcaptain23/browser-android-tabs

Conflicts:
	.gn
	third_party/WebKit/Source/core/html/HTMLCanvasElement.h

third_party/WebKit/Source/core/html/HTMLCanvasElement.h
@@ -250,6 +250,7 @@ class CORE_EXPORT HTMLCanvasElement final
   void DetachContext() override { context_ = nullptr; }
 
   void WillDrawImageTo2DContext(CanvasImageSource*);
+  bool wasBlockedByFingerprinting() { return wasBlockedByFingerprinting_; }
 
   ExecutionContext* GetTopExecutionContext() const override {
     return GetDocument().GetExecutionContext();