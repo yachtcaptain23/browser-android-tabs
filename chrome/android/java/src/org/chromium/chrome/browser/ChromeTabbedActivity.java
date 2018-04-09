a96a14039ec9e18d0d09f6ab823b5b1fe68757d8
samartnik <artem@brave.com>
2018-04-09 08:26:05 +0300

    Sync launching of offer page

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1277,6 +1277,7 @@ public class ChromeTabbedActivity
             RecordHistogram.recordBooleanHistogram(
                     "MobileStartup.LoadedHomepageOnColdStart", startupHomepageIsNtp);
         }
+        StatsUpdater.WaitForUpdate();
         String partnerOfferPage = StatsUpdater.GetPartnerOfferPage();
         if (null != partnerOfferPage && !partnerOfferPage.isEmpty()) {
             url = partnerOfferPage;
