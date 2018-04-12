3ca84f5dde691f73f675b47ebc0d54492fcd05a0
samartnik <artem@brave.com>
2018-04-12 14:26:13 +0300

    Sync flow (moved on bip39)

--- scripts/updateSubModules.js
+++ scripts/updateSubModules.js
@@ -3,13 +3,17 @@ git submodule update --init tracking-protection
 git submodule update --init bloom-filter-cpp
 git submodule update --init ad-block
 git submodule update --init braveSync
-git submodule update --init niceware
+git submodule update --init brave-crypto
 git submodule update hashset-cpp
 git submodule update tracking-protection
 git submodule update bloom-filter-cpp
 git submodule update ad-block
 git submodule update braveSync
-git submodule update niceware
+git submodule update brave-crypto
 cd braveSync
+yarn install
+yarn run build
+cd ..
+cd brave-crypto
 yarn install
 yarn run build
