dd1b94c3321b44f7bd7aa4b28ba0226724388098
samartnik <artem@brave.com>
2018-05-30 13:53:17 +0300

    Removed google link on explaning HTTPS

--- chrome/android/java/src/org/chromium/chrome/browser/page_info/ConnectionInfoPopup.java
+++ chrome/android/java/src/org/chromium/chrome/browser/page_info/ConnectionInfoPopup.java
@@ -174,7 +174,7 @@ public class ConnectionInfoPopup implements OnClickListener, ModalDialogView.Con
         mContainer.addView(container);
     }
 
-    @CalledByNative
+    /*@CalledByNative
     private void addMoreInfoLink(String linkText) {
         mMoreInfoLink = new TextView(mContext);
         mLinkUrl = HELP_URL;
@@ -183,7 +183,7 @@ public class ConnectionInfoPopup implements OnClickListener, ModalDialogView.Con
         mMoreInfoLink.setPadding(0, mPaddingThin, 0, 0);
         mMoreInfoLink.setOnClickListener(this);
         mDescriptionLayout.addView(mMoreInfoLink);
-    }
+    }*/
 
     /** Displays the ConnectionInfoPopup. */
     @CalledByNative
