74a546b1ec8bda05d6804bc07157c631e15da2c1
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-06-21 16:28:39 -0400

    Default behaviour correction

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceManager.java
@@ -588,7 +588,7 @@ public class ChromePreferenceManager {
      * @return True if we can use custom tabs.
      */
     public boolean useCustomTabs() {
-        return mSharedPreferences.getBoolean(USE_CUSTOM_TABS, false);
+        return mSharedPreferences.getBoolean(USE_CUSTOM_TABS, true);
     }
 
     /**
