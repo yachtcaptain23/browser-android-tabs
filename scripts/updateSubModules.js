07258a4209903e7fe324cc0ba44f747767f0dae3
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-06-21 23:18:29 -0400

    Submodule dependencies

--- scripts/updateSubModules.js
+++ scripts/updateSubModules.js
@@ -4,12 +4,20 @@ git submodule update --init bloom-filter-cpp
 git submodule update --init ad-block
 git submodule update --init braveSync
 git submodule update --init brave-crypto
+git submodule update --init niceware
+git submodule update --init anonize2
+git submodule update --init bip39wally-core
+git submodule update --init braveLedger
 git submodule update hashset-cpp
 git submodule update tracking-protection
 git submodule update bloom-filter-cpp
 git submodule update ad-block
 git submodule update braveSync
 git submodule update brave-crypto
+git submodule update niceware
+git submodule update anonize2
+git submodule update bip39wally-core
+git submodule update braveLedger
 cd braveSync
 yarn install
 yarn run build
