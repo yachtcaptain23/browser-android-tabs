92087ca245384679cb72a34ddcdb71b767d0fd06
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-07-10 21:39:52 -0400

    Implement BraveRewardsService

---
+++ chrome/browser/braveRewards/brave_rewards_service_impl.cc
@@ -0,0 +1,277 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+#include "brave_rewards_service_impl.h"
+
+#include "base/bind.h"
+#include "base/guid.h"
+#include "base/files/file_util.h"
+#include "base/files/important_file_writer.h"
+#include "base/sequenced_task_runner.h"
+#include "base/task_runner_util.h"
+#include "base/task_scheduler/post_task.h"
+#include "base/threading/sequenced_task_runner_handle.h"
+#include "bat/ledger/ledger.h"
+#include "brave_rewards_service_observer.h"
+#include "chrome/browser/browser_process_impl.h"
+#include "chrome/browser/profiles/profile.h"
+#include "net/url_request/url_fetcher.h"
+#include "url/gurl.h"
+
+// TODO, just for test purpose
+static bool created_wallet = false;
+//
+
+namespace brave_rewards {
+
+namespace {
+
+class LedgerURLLoaderImpl : public ledger::LedgerURLLoader {
+ public:
+  LedgerURLLoaderImpl(uint64_t request_id, net::URLFetcher* fetcher) :
+    request_id_(request_id),
+    fetcher_(fetcher) {}
+  ~LedgerURLLoaderImpl() override = default;
+
+  void Start() override {
+    fetcher_->Start();
+  }
+
+  uint64_t request_id() override {
+    return request_id_;
+  }
+
+private:
+  uint64_t request_id_;
+  net::URLFetcher* fetcher_;  // NOT OWNED
+};
+
+net::URLFetcher::RequestType URLMethodToRequestType(ledger::URL_METHOD method) {
+  switch(method) {
+    case ledger::URL_METHOD::GET:
+      return net::URLFetcher::RequestType::GET;
+    case ledger::URL_METHOD::POST:
+      return net::URLFetcher::RequestType::POST;
+    case ledger::URL_METHOD::PUT:
+      return net::URLFetcher::RequestType::PUT;
+    default:
+      NOTREACHED();
+      return net::URLFetcher::RequestType::GET;
+  }
+}
+
+std::string LoadStateOnFileTaskRunner(
+    const base::FilePath& path) {
+  std::string data;
+  bool success = base::ReadFileToString(path, &data);
+
+  // Make sure the file isn't empty.
+  if (!success || data.empty()) {
+    LOG(ERROR) << "Failed to read file: " << path.MaybeAsASCII();
+    return std::string();
+  }
+  return data;
+}
+
+// `callback` has a WeakPtr so this won't crash if the file finishes
+// writing after BraveRewardsServiceImpl has been destroyed
+void PostWriteCallback(
+    const base::Callback<void(bool success)>& callback,
+    scoped_refptr<base::SequencedTaskRunner> reply_task_runner,
+    bool write_success) {
+  // We can't run |callback| on the current thread. Bounce back to
+  // the |reply_task_runner| which is the correct sequenced thread.
+  reply_task_runner->PostTask(FROM_HERE,
+                              base::Bind(callback, write_success));
+}
+
+static uint64_t next_id = 1;
+
+}  // namespace
+
+BraveRewardsServiceImpl::BraveRewardsServiceImpl(Profile* profile) :
+    profile_(profile),
+    ledger_(ledger::Ledger::CreateInstance(this)),
+    file_task_runner_(base::CreateSequencedTaskRunnerWithTraits(
+        {base::MayBlock(), base::TaskPriority::BACKGROUND,
+         base::TaskShutdownBehavior::BLOCK_SHUTDOWN})),
+    ledger_state_path_(profile_->GetPath().Append("ledger_state")),
+    publisher_state_path_(profile_->GetPath().Append("publisher_state")) {
+}
+
+BraveRewardsServiceImpl::~BraveRewardsServiceImpl() {
+}
+
+void BraveRewardsServiceImpl::CreateWallet() {
+  if (created_wallet) {
+    return;
+  }
+  ledger_->CreateWallet();
+  created_wallet = true;
+}
+
+std::string BraveRewardsServiceImpl::GenerateGUID() const {
+  return base::GenerateGUID();
+}
+
+void BraveRewardsServiceImpl::Shutdown() {
+  fetchers_.clear();
+  ledger_.reset();
+  BraveRewardsService::Shutdown();
+}
+
+void BraveRewardsServiceImpl::OnWalletCreated(ledger::Result result) {
+  TriggerOnWalletCreated(result);
+}
+
+void BraveRewardsServiceImpl::OnReconcileComplete(ledger::Result result,
+                                              const std::string& viewing_id) {
+  LOG(ERROR) << "reconcile complete " << viewing_id;
+}
+
+void BraveRewardsServiceImpl::LoadLedgerState(
+    ledger::LedgerCallbackHandler* handler) {
+  base::PostTaskAndReplyWithResult(file_task_runner_.get(), FROM_HERE,
+      base::Bind(&LoadStateOnFileTaskRunner, ledger_state_path_),
+      base::Bind(&BraveRewardsServiceImpl::OnLedgerStateLoaded,
+                     AsWeakPtr(),
+                     base::Unretained(handler)));
+}
+
+void BraveRewardsServiceImpl::OnLedgerStateLoaded(
+    ledger::LedgerCallbackHandler* handler,
+    const std::string& data) {
+  handler->OnLedgerStateLoaded(data.empty() ? ledger::Result::ERROR
+                                            : ledger::Result::OK,
+                               data);
+}
+
+void BraveRewardsServiceImpl::LoadPublisherState(
+    ledger::LedgerCallbackHandler* handler) {
+  base::PostTaskAndReplyWithResult(file_task_runner_.get(), FROM_HERE,
+      base::Bind(&LoadStateOnFileTaskRunner, publisher_state_path_),
+      base::Bind(&BraveRewardsServiceImpl::OnPublisherStateLoaded,
+                     AsWeakPtr(),
+                     base::Unretained(handler)));
+}
+
+void BraveRewardsServiceImpl::OnPublisherStateLoaded(
+    ledger::LedgerCallbackHandler* handler,
+    const std::string& data) {
+  handler->OnPublisherStateLoaded(data.empty() ? ledger::Result::ERROR
+                                               : ledger::Result::OK,
+                                  data);
+}
+
+void BraveRewardsServiceImpl::SaveLedgerState(const std::string& ledger_state,
+                                      ledger::LedgerCallbackHandler* handler) {
+  base::ImportantFileWriter writer(
+      ledger_state_path_, file_task_runner_);
+
+  writer.RegisterOnNextWriteCallbacks(
+      base::Closure(),
+      base::Bind(
+        &PostWriteCallback,
+        base::Bind(&BraveRewardsServiceImpl::OnLedgerStateSaved, AsWeakPtr(),
+            base::Unretained(handler)),
+        base::SequencedTaskRunnerHandle::Get()));
+
+  writer.WriteNow(std::make_unique<std::string>(ledger_state));
+}
+
+void BraveRewardsServiceImpl::OnLedgerStateSaved(
+    ledger::LedgerCallbackHandler* handler,
+    bool success) {
+  handler->OnLedgerStateSaved(success ? ledger::Result::OK
+                                      : ledger::Result::ERROR);
+}
+
+void BraveRewardsServiceImpl::SavePublisherState(const std::string& publisher_state,
+                                      ledger::LedgerCallbackHandler* handler) {
+  base::ImportantFileWriter writer(publisher_state_path_, file_task_runner_);
+
+  writer.RegisterOnNextWriteCallbacks(
+      base::Closure(),
+      base::Bind(
+        &PostWriteCallback,
+        base::Bind(&BraveRewardsServiceImpl::OnPublisherStateSaved, AsWeakPtr(),
+            base::Unretained(handler)),
+        base::SequencedTaskRunnerHandle::Get()));
+
+  writer.WriteNow(std::make_unique<std::string>(publisher_state));
+}
+
+void BraveRewardsServiceImpl::OnPublisherStateSaved(
+    ledger::LedgerCallbackHandler* handler,
+    bool success) {
+  handler->OnPublisherStateSaved(success ? ledger::Result::OK
+                                         : ledger::Result::ERROR);
+}
+
+std::unique_ptr<ledger::LedgerURLLoader> BraveRewardsServiceImpl::LoadURL(const std::string& url,
+                 const std::vector<std::string>& headers,
+                 const std::string& content,
+                 const std::string& contentType,
+                 const ledger::URL_METHOD& method,
+                 ledger::LedgerCallbackHandler* handler) {
+  net::URLFetcher::RequestType request_type = URLMethodToRequestType(method);
+
+  net::URLFetcher* fetcher = net::URLFetcher::Create(
+      GURL(url), request_type, this).release();
+  fetcher->SetRequestContext(g_browser_process->system_request_context());
+
+  for (size_t i = 0; i < headers.size(); i++)
+    fetcher->AddExtraRequestHeader(headers[i]);
+
+  if (!content.empty())
+    fetcher->SetUploadData(contentType, content);
+
+  FetchCallback callback = base::Bind(
+      &ledger::LedgerCallbackHandler::OnURLRequestResponse,
+      base::Unretained(handler),
+      next_id);
+  fetchers_[fetcher] = callback;
+
+  std::unique_ptr<ledger::LedgerURLLoader> loader(
+      new LedgerURLLoaderImpl(next_id++, fetcher));
+
+  return loader;
+}
+
+void BraveRewardsServiceImpl::OnURLFetchComplete(
+    const net::URLFetcher* source) {
+  if (fetchers_.find(source) == fetchers_.end())
+    return;
+
+  auto callback = fetchers_[source];
+  fetchers_.erase(source);
+
+  int response_code = source->GetResponseCode();
+  std::string body;
+  if (response_code != net::URLFetcher::ResponseCode::RESPONSE_CODE_INVALID &&
+      source->GetStatus().is_success()) {
+    source->GetResponseAsString(&body);
+  }
+
+  callback.Run(response_code, body);
+}
+
+void BraveRewardsServiceImpl::RunIOTask(
+    std::unique_ptr<ledger::LedgerTaskRunner> task) {
+  file_task_runner_->PostTask(FROM_HERE,
+      base::BindOnce(&ledger::LedgerTaskRunner::Run, std::move(task)));
+}
+
+void BraveRewardsServiceImpl::RunTask(
+      std::unique_ptr<ledger::LedgerTaskRunner> task) {
+  content::BrowserThread::PostTask(content::BrowserThread::UI, FROM_HERE,
+      base::BindOnce(&ledger::LedgerTaskRunner::Run,
+                     std::move(task)));
+}
+
+void BraveRewardsServiceImpl::TriggerOnWalletCreated(int error_code) {
+  for (auto& observer : observers_)
+    observer.OnWalletCreated(this, error_code);
+}
+
+}  // namespace brave_rewards
