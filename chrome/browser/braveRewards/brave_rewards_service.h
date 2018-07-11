3faab53263b3076d6be5059868d50c4afadac498
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-07-10 21:39:52 -0400

    Implement BraveRewardsService

---
+++ chrome/browser/braveRewards/brave_rewards_service.h
@@ -0,0 +1,38 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_REWARDS_SERVICE_
+#define BRAVE_REWARDS_SERVICE_
+
+#include "base/macros.h"
+#include "base/observer_list.h"
+#include "components/keyed_service/core/keyed_service.h"
+
+namespace brave_rewards {
+
+class BraveRewardsServiceObserver;
+
+class BraveRewardsService : public KeyedService {
+public:
+  BraveRewardsService();
+  ~BraveRewardsService() override;
+
+  // KeyedService:
+  void Shutdown() override;
+
+  virtual void CreateWallet() = 0;
+
+  void AddObserver(BraveRewardsServiceObserver* observer);
+  void RemoveObserver(BraveRewardsServiceObserver* observer);
+
+protected:
+  base::ObserverList<BraveRewardsServiceObserver> observers_;
+
+private:
+  DISALLOW_COPY_AND_ASSIGN(BraveRewardsService);
+};
+
+}  // namespace brave_rewards
+
+#endif  // BRAVE_REWARDS_SERVICE_
