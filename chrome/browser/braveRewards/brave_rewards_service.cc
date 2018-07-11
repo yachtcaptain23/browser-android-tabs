a45df92f8dd12928f291511ff0736192a03ca3c3
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-07-10 21:39:52 -0400

    Implement BraveRewardsService

---
+++ chrome/browser/braveRewards/brave_rewards_service.cc
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave_rewards_service.h"
+
+namespace brave_rewards {
+
+BraveRewardsService::BraveRewardsService() {
+}
+
+BraveRewardsService::~BraveRewardsService() {
+}
+
+void BraveRewardsService::Shutdown() {
+}
+
+}  // namespace brave_rewards
