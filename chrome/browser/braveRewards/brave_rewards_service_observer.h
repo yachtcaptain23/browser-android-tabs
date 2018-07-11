6a2a0c1ff0a6bc77f60e31ea82cac053bfb0f900
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-07-10 21:39:52 -0400

    Implement BraveRewardsService

---
+++ chrome/browser/braveRewards/brave_rewards_service_observer.h
@@ -0,0 +1,22 @@
+ /* This Source Code Form is subject to the terms of the Mozilla Public
+  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+  * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_REWARDS_SERVICE_OBSERVER_H_
+#define BRAVE_REWARDS_SERVICE_OBSERVER_H_
+
+namespace brave_rewards {
+
+class BraveRewardsService;
+
+class BraveRewardsServiceObserver {
+ public:
+  virtual ~BraveRewardsServiceObserver() {}
+
+  virtual void OnWalletCreated(BraveRewardsService* payment_service,
+                               int error_code) {};
+};
+
+}  // namespace brave_rewards
+
+#endif  // BRAVE_REWARDS_SERVICE_OBSERVER_H_
