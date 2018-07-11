d0dc43ceb8ebf2c8898883c4659865c5626a4f7e
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-07-10 21:39:52 -0400

    Implement BraveRewardsService

---
+++ chrome/browser/braveRewards/brave_rewards_service_factory.h
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_REWARDS_SERVICE_FACTORY_H_
+#define BRAVE_REWARDS_SERVICE_FACTORY_H_
+
+#include "base/memory/singleton.h"
+#include "components/keyed_service/content/browser_context_keyed_service_factory.h"
+
+class Profile;
+
+namespace brave_rewards {
+class BraveRewardsService;
+}
+
+// Singleton that owns all BraveRewardsService and associates them with
+// Profiles.
+class BraveRewardsServiceFactory : public BrowserContextKeyedServiceFactory {
+ public:
+  static brave_rewards::BraveRewardsService* GetForProfile(Profile* profile);
+
+  static BraveRewardsServiceFactory* GetInstance();
+
+ private:
+  friend struct base::DefaultSingletonTraits<BraveRewardsServiceFactory>;
+
+  BraveRewardsServiceFactory();
+  ~BraveRewardsServiceFactory() override;
+
+  // BrowserContextKeyedServiceFactory:
+  KeyedService* BuildServiceInstanceFor(
+      content::BrowserContext* context) const override;
+  content::BrowserContext* GetBrowserContextToUse(
+      content::BrowserContext* context) const override;
+  bool ServiceIsNULLWhileTesting() const override;
+};
+
+#endif  // BRAVE_REWARDS_SERVICE_FACTORY_H_
