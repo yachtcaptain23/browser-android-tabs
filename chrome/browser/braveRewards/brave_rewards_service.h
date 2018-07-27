2e74897e1ee2b0298451ccc7b6368a34dbac785e
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-07-27 15:51:08 -0400

    ledger leveldb publishers storage

--- chrome/browser/braveRewards/brave_rewards_service.h
+++ chrome/browser/braveRewards/brave_rewards_service.h
@@ -7,11 +7,16 @@
 
 #include "base/macros.h"
 #include "base/observer_list.h"
+#include "content_site.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 namespace brave_rewards {
 
 class BraveRewardsServiceObserver;
+
+using GetContentSiteListCallback =
+    base::Callback<void(std::unique_ptr<ContentSiteList>,
+        uint32_t /* next_record */)>;
 
 class BraveRewardsService : public KeyedService {
 public:
@@ -35,6 +40,9 @@ public:
   virtual unsigned int GetPublisherMinVisits() const = 0;
   virtual bool GetPublisherAllowNonVerified() const = 0;
   virtual double GetContributionAmount() const = 0;
+  virtual void GetContentSiteList(uint32_t start,
+                                uint32_t limit,
+                                const GetContentSiteListCallback& callback) = 0;
 
   void AddObserver(BraveRewardsServiceObserver* observer);
   void RemoveObserver(BraveRewardsServiceObserver* observer);
