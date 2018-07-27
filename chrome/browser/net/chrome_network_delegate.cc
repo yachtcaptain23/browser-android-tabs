d91ea3705508fccd5629d14fef0a4504415f05b1
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-07-27 16:22:42 -0400

    make ledger buildable

--- chrome/browser/net/chrome_network_delegate.cc
+++ chrome/browser/net/chrome_network_delegate.cc
@@ -88,7 +88,7 @@ bool g_access_to_all_files_enabled = false;
 // Notifies ledger lib about media links
 void NotifyLedgerIOThread(IOThread* io_thread, const std::string& url, 
       const std::string& urlQuery, const std::string& type, bool privateTab) {
-  io_thread->globals()->ledger_->OnMediaRequest(url, urlQuery, type/*, privateTab*/);
+  //io_thread->globals()->ledger_->OnMediaRequest(url, urlQuery, type/*, privateTab*/);
 }
 
 bool IsAccessAllowedInternal(const base::FilePath& path,
