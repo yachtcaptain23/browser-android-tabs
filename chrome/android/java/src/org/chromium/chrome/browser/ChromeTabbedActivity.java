5a194b80ab6278484188837ec3c10ab201a0a241
samartnik <artem@brave.com>
2018-07-31 19:12:32 +0300

    Fixed build for cr-68

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1561,7 +1561,12 @@ public class ChromeTabbedActivity
                     setStatusBarColor(null, ColorUtils.getDefaultThemeColor(
                             getResources(), newModel.isIncognito()));
                 }
+            }
+        });
 
+        mTabModelSelectorTabObserver = new TabModelSelectorTabObserver(mTabModelSelectorImpl) {
+            @Override
+            public void onPageLoadStarted(Tab tab, String url) {
                 ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
                 if ((null != app) && (null != app.getShieldsConfig())) {
                     app.getShieldsConfig().setTabModelSelectorTabObserver(mTabModelSelectorTabObserver);
@@ -1578,7 +1583,7 @@ public class ChromeTabbedActivity
             }
 
             @Override
-            public void onPageLoadFinished(Tab tab) {
+            public void onPageLoadFinished(final Tab tab) {
                 String url = tab.getUrl();
                 if (getActivityTab() == tab) {
                     try {
--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -3581,7 +3581,7 @@ WebGLRenderingContextBase::getSupportedExtensions() {
       }
   }
   if (!allowed)
-      return WTF::nullopt;
+      return base::nullopt;
 
   Vector<String> result;
 
