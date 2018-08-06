244a998867a56086846cb71be2a854d29b095294
samartnik <artem@brave.com>
2018-08-06 12:55:15 +0300

    UI changes for Private tabs

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/IncognitoNewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/IncognitoNewTabPageView.java
@@ -38,6 +38,7 @@ public class IncognitoNewTabPageView extends HistoryNavigationLayout {
     private int mSnapshotScrollY;
 
     private View mDDGOfferLink;
+    private View mDDGOfferImage;
 
     /**
      * Manages the view interaction with the rest of the system.
@@ -90,6 +91,15 @@ public class IncognitoNewTabPageView extends HistoryNavigationLayout {
                 showDDGOffer(true);
             }
         });
+
+        mDDGOfferImage = findViewById(R.id.ddg_offer_img);
+        mDDGOfferImage.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDDGOffer(true);
+            }
+        });
+
         showDDGOffer(false);
     }
 
@@ -142,6 +152,7 @@ public class IncognitoNewTabPageView extends HistoryNavigationLayout {
     public void showDDGOffer(boolean forceShow) {
         if (TemplateUrlService.getInstance().getDefaultSearchEngineKeyword(true).equals(INCOGNITO_DSE_KEYWORD)) {
             mDDGOfferLink.setVisibility(View.GONE);
+            mDDGOfferImage.setVisibility(View.GONE);
             return;
         }
         if (!forceShow && ContextUtils.getAppSharedPreferences().getBoolean(PREF_DDG_OFFER_SHOWN, false)) {
@@ -155,6 +166,7 @@ public class IncognitoNewTabPageView extends HistoryNavigationLayout {
             public void onClick(DialogInterface dialog, int which) {
                 TemplateUrlService.getInstance().setSearchEngine(INCOGNITO_DSE_NAME, INCOGNITO_DSE_KEYWORD, true);
                 mDDGOfferLink.setVisibility(View.GONE);
+                mDDGOfferImage.setVisibility(View.GONE);
             }
         })
         .setNegativeButton(R.string.ddg_offer_negative, null)
