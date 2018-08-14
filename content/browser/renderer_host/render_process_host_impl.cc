2292e02fb3099d494f581d5c0f8bd0d4d8b84f1d
samartnik <artem@brave.com>
2018-08-14 16:31:49 +0300

    Fixed DDG UA

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -2710,6 +2710,10 @@ void RenderProcessHostImpl::AddFrameWithSite(
     BrowserContext* browser_context,
     RenderProcessHost* render_process_host,
     const GURL& site_url) {
+  if (render_process_host && render_process_host->GetRendererInterface() && GetContentClient()) {
+    render_process_host->GetRendererInterface()->SetUserAgent(GetContentClient()->GetUserAgent(site_url.host()));
+  }
+
   if (!ShouldTrackProcessForSite(browser_context, render_process_host,
                                  site_url))
     return;
