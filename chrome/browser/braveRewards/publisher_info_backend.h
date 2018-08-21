ec5931e08b0211afb7762d9e13f812e805647a69
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-08-20 23:16:17 -0400

    Ledger multiple publishers list support

--- chrome/browser/braveRewards/publisher_info_backend.h
+++ chrome/browser/braveRewards/publisher_info_backend.h
@@ -24,6 +24,8 @@ class PublisherInfoBackend {
   bool Get(const std::string& lookup, std::string* value);
   bool Load(uint32_t start, uint32_t limit,
             std::vector<const std::string>& results);
+  bool LoadWithPrefix(uint32_t start, uint32_t limit, const std::vector<std::string>& prefix,
+            std::vector<const std::string>& results);
 
  private:
   bool EnsureInitialized();
