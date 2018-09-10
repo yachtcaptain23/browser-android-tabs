8fa9271f4a45e1cb20088df360775758f712f05f
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-09-10 14:09:20 -0400

    package.json for brave rewards ui

---
+++ components/webpack/prod.config.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    brave_rewards: path.join(__dirname, '../brave_rewards_ui/resources/brave_rewards'),
+  },
+  output: {
+    path: process.env.TARGET_GEN_DIR,
+    filename: '[name].bundle.js',
+    chunkFilename: '[id].chunk.js'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.IgnorePlugin(/[^/]+\/[\S]+.dev$/),
+    new UglifyJSPlugin({
+      uglifyOptions: {
+        warnings: false,
+        output: {
+          comments: false
+        },
+        compress: {
+          warnings: false
+        }
+      }
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.tsx', '.ts', '.json'],
+    alias: {
+      'dgram': 'chrome-dgram',
+      'net': 'chrome-net'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react-optimize']
+        }
+      },
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader?-minimize!less-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?-minimize'
+      },
+      // Loads font files for Font Awesome
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=13000&minetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg|png|jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      }]
+  },
+  node: {
+    fs: 'empty'
+  }
+}
