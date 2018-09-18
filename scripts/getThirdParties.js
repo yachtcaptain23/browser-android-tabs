7a0f54fe7eececf5090b920c7615ab1835de2dae
samartnik <artem@brave.com>
2018-09-18 19:50:25 +0300

    Fixed third parties script

--- scripts/getThirdParties.js
+++ scripts/getThirdParties.js
@@ -3,8 +3,8 @@ cp -f scripts/.gclient_entries ../.gclient_entries
 gclient sync --with_branch_heads
 cd ..
 echo "{ 'GYP_DEFINES': 'OS=android target_arch=arm buildtype=Official', }" > chromium.gyp_env
-gclient runhooks
 cd src
+gclient runhooks
 build/install-build-deps-android.sh
 gclient sync
 sh . build/android/envsetup.sh
