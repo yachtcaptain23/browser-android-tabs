5546617d8171aeb60ac0bc6fed4d747d955bb4a1
samartnik <artem@brave.com>
2018-09-24 17:22:59 +0300

    Fixed stats

--- chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
@@ -144,7 +144,17 @@ public class ChromeBrowserInitializer {
           return;
       }
       mUpdateStatsCalled = true;
-      new UpdateStatsAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+      new Thread(new Runnable() {
+          @Override
+          public void run () {
+              try {
+                  StatsUpdater.UpdateStats(ContextUtils.getApplicationContext());
+              }
+              catch(Exception exc) {
+                  // Just ignore it if we cannot update
+              }
+          }
+      }).start();
     }
 
     private void CheckInstallationSource() {
@@ -171,21 +181,6 @@ public class ChromeBrowserInitializer {
         protected Long doInBackground(Void... params) {
             try {
                 ADBlockUpdater.UpdateADBlock(ContextUtils.getApplicationContext(), true);
-            }
-            catch(Exception exc) {
-                // Just ignore it if we cannot update
-            }
-
-            return null;
-        }
-    }
-
-    // Stats update
-    class UpdateStatsAsyncTask extends AsyncTask<Void,Void,Long> {
-        @Override
-        protected Long doInBackground(Void... params) {
-            try {
-                StatsUpdater.UpdateStats(ContextUtils.getApplicationContext());
             }
             catch(Exception exc) {
                 // Just ignore it if we cannot update
@@ -584,7 +579,7 @@ public class ChromeBrowserInitializer {
         }
 
         InitAdBlock();
-        //UpdateStats();
+        UpdateStats();
         CheckInstallationSource();
         SwitchSearchSuggestEnabled();
     }
