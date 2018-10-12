89e82ffb6c0c46be45d4e89f80453ccfec1bcffa
Albert Wang <amwang217@gmail.com>
2018-10-11 18:39:15 -0700

    Broken commit for new tab

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/BottomToolbarCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/BottomToolbarCoordinator.java
@@ -28,6 +28,7 @@ import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.toolbar.BottomToolbarViewBinder.ViewHolder;
 import org.chromium.chrome.browser.toolbar.ToolbarButtonSlotData.ToolbarButtonData;
 import org.chromium.chrome.browser.widget.TintedImageButton;
+import org.chromium.chrome.browser.widget.newtab.NewTabButton;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.resources.ResourceManager;
 
@@ -49,6 +50,9 @@ public class BottomToolbarCoordinator {
 
     /** The history button component that lives in the bottom toolbar. */
     private final TintedImageButton mHistoryButton;
+
+    /** The new tab button component that lives in the bottom toolbar. */
+    private final NewTabButton mNewTabButton;
 
     /** The menu button that lives in the bottom toolbar. */
     private final MenuButton mMenuButton;
@@ -117,6 +121,7 @@ public class BottomToolbarCoordinator {
                 firstSlotData, secondSlotData, mNormalPrimaryColor);
         mActivity = activity;
         mToolbarModel = toolbarModel;
+        mNewTabButton = toolbarRoot.findViewById(R.id.new_tab_button);
     }
 
     /**
