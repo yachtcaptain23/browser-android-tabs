d6ea5bc53405aee76d7841082fa5f43bcbe0f901
Albert Wang <amwang217@gmail.com>
2018-10-25 23:41:53 -0700

    Not breaking change to show text

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -57,6 +57,7 @@ import org.chromium.base.VisibleForTesting;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.BraveBadge;
 import org.chromium.chrome.browser.BraveSyncWorker;
 import org.chromium.chrome.browser.StatsUpdaterWorker;
 import org.chromium.chrome.browser.ADBlockUpdaterWorker;
@@ -1645,6 +1646,8 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             app.mADBlockUpdaterWorker = new ADBlockUpdaterWorker(this);
             //MixPanelWorker.SendBraveAppStartEvent();
         }
+        BraveBadge braveRewardsBadge = (BraveBadge) findViewById(R.id.brave_rewards_badge);
+        braveRewardsBadge.update(this, 5);
     }
 
     /**
