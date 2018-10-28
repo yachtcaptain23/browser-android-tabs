5f76d39bfd42a9a3d0748032dca025ed1d162826
Albert Wang <amwang217@gmail.com>
2018-10-28 13:01:36 -0700

    Fixed badge layout

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1711,8 +1711,6 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
                 app.mStatsUpdaterWorker = new StatsUpdaterWorker(app);
             }
         }
-        BraveBadge braveRewardsBadge = (BraveBadge) findViewById(R.id.brave_rewards_badge);
-        braveRewardsBadge.update(this, 5);
     }
 
     /**
