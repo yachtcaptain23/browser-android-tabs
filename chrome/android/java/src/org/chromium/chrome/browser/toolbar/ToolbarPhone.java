2d55005a81cfef5177833048a1d7b605dc6e1fcf
Albert Wang <amwang217@gmail.com>
2018-10-28 14:42:14 -0700

    Don't show the toolbar when in tab switcher

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
@@ -1071,7 +1071,7 @@ public class ToolbarPhone extends ToolbarLayout
      */
     protected void updateUrlExpansionAnimation() {
         if (isInTabSwitcherMode()) {
-            mToolbarButtonsContainer.setVisibility(VISIBLE);
+            mToolbarButtonsContainer.setVisibility(INVISIBLE);
             return;
         }
 
