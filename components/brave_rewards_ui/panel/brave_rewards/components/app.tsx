7eecd2b43f7c37514f59c327ecbe6b50459bbb5e
Peter Miller <miller.pete@gmail.com>
2018-11-06 01:10:08 -0800

    Optimize rewards JS bundle and split to chunks

--- components/brave_rewards_ui/panel/brave_rewards/components/app.tsx
+++ components/brave_rewards_ui/panel/brave_rewards/components/app.tsx
@@ -2,16 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import * as React from 'react'
+// @ts-ignore until react type includes Suspense
+import React, { Suspense } from 'react'
 import { bindActionCreators, Dispatch } from 'redux'
 import { connect } from 'react-redux'
+
+import * as rewardsPanelActions from '../actions/rewards_panel_actions'
 import { PanelWelcome } from 'brave-ui/features/rewards'
 
-// Components
-import Panel from './panel'
-
-// Utils
-import * as rewardsPanelActions from '../actions/rewards_panel_actions'
+const Panel = React.lazy<any>(() => import('./panel'))
 
 interface Props extends RewardsExtension.ComponentProps {
 }
@@ -69,7 +68,7 @@ export class RewardsPanel extends React.Component<Props, State> {
     const currentTabUrl = rewardsPanelData.currentTabUrl || ''
 
     return (
-      <>
+      <React.Fragment>
         {
           !walletCreated
           ? <PanelWelcome
@@ -80,13 +79,15 @@ export class RewardsPanel extends React.Component<Props, State> {
             optInAction={this.onCreate}
           />
           : currentTabId && currentTabUrl
-            ? <Panel
-                url={currentTabUrl}
-                tabId={currentTabId}
-            />
+            ? <Suspense fallback={<div />}>
+                <Panel
+                  url={currentTabUrl}
+                  tabId={currentTabId}
+                />
+              </Suspense>
             : null
         }
-      </>
+      </React.Fragment>
     )
   }
 }
