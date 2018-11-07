542fcb6053e3318f23b9412e689029051c3670ef
ryanml <ryanlanese@gmail.com>
2018-11-07 16:00:46 -0700

    Preparing for publisherData handler

--- components/brave_rewards_ui/panel/brave_rewards/constants/rewards_panel_types.ts
+++ components/brave_rewards_ui/panel/brave_rewards/constants/rewards_panel_types.ts
@@ -8,6 +8,7 @@ export const enum types {
   ON_WALLET_CREATE_FAILED = '@@rewards_panel/ON_WALLET_CREATE_FAILED',
   ON_TAB_ID = '@@rewards_panel/ON_TAB_ID',
   ON_TAB_RETRIEVED = '@@rewards_panel/ON_TAB_RETRIEVED',
+  GET_PUBLISHER_DATA = '@@rewards_panel/GET_PUBLISHER_DATA',
   ON_PUBLISHER_DATA = '@@rewards_panel/ON_PUBLISHER_DATA',
   GET_WALLET_PROPERTIES = '@@rewards_panel/GET_WALLET_PROPERTIES',
   ON_WALLET_PROPERTIES = '@@rewards_panel/ON_WALLET_PROPERTIES',
