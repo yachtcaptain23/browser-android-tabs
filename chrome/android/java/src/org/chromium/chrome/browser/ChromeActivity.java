0adec30e23feef9a71957a8bc855cea1ba76613e
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-11-15 11:47:29 -0500

    Donate HTML page

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -2528,8 +2528,6 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             DomDistillerUIUtils.openSettings(currentTab.getWebContents());
         } else if (id == R.id.brave_set_default_browser) {
           handleBraveSetDefaultBrowserDialog();
-        } else if (id == R.id.brave_rewards_button) {
-            showBraveRewardsPanel();
         } else if (id == R.id.exit_id) {
             ApplicationLifetime.terminate(false);
         } else {
@@ -2602,10 +2600,6 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
                 return;
             }
         }
-    }
-
-    private void showBraveRewardsPanel() {
-        PopupActivity.show(this);
     }
 
     /**
