72b476e5c57da385427076daf9d60405b69aff83
Peter Miller <miller.pete@gmail.com>
2018-11-17 22:54:05 -0800

    Generate GRD and PAK files based on dynamic webpack output

--- chrome/browser/ui/webui/brave_rewards_donate_ui.h
+++ chrome/browser/ui/webui/brave_rewards_donate_ui.h
@@ -4,7 +4,7 @@
 #ifndef BRAVE_BROWSER_UI_WEBUI_REWARDS_DONATE_UI_H_
 #define BRAVE_BROWSER_UI_WEBUI_REWARDS_DONATE_UI_H_
 
-#include "chrome/browser/ui/webui/basic_ui.h"
+#include "brave/browser/ui/webui/basic_ui.h"
 
 class BraveRewardsDonateUI : public BasicUI {
  public:
