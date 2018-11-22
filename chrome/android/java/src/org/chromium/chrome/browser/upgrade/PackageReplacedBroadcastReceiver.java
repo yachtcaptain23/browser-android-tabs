fc98a924c65ca348d23c95a607e535bf42e2d50f
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-11-22 15:20:00 -0500

    Update notification

--- chrome/android/java/src/org/chromium/chrome/browser/upgrade/PackageReplacedBroadcastReceiver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/upgrade/PackageReplacedBroadcastReceiver.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 
+import org.chromium.base.Log;
 import org.chromium.base.task.AsyncTask;
 import org.chromium.chrome.browser.notifications.channels.ChannelsUpdater;
 
@@ -29,8 +30,16 @@ import org.chromium.chrome.browser.notifications.channels.ChannelsUpdater;
 public final class PackageReplacedBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
+        Log.i("TAG", "!!!onReceive");
         if (!Intent.ACTION_MY_PACKAGE_REPLACED.equals(intent.getAction())) return;
+        Log.i("TAG", "!!!afterReceive");
         updateChannelsIfNecessary();
+        try {
+            NotificationIntent.fireNotificationIfNecessary(context);
+        } catch (Exception exc) {
+            // Just ignore if we could not send a notification
+            Log.i("TAG", "!!!notification error " + exc.getMessage());
+        }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) return;
         UpgradeIntentService.startMigrationIfNecessary(context);
     }
