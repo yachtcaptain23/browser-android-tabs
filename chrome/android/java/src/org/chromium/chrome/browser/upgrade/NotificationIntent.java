90ac3ff4ac26dcf63fd15f76aacfd0190ed8e85e
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-11-23 12:02:05 -0500

    Spanish translation

--- chrome/android/java/src/org/chromium/chrome/browser/upgrade/NotificationIntent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/upgrade/NotificationIntent.java
@@ -36,7 +36,7 @@ public class NotificationIntent {
     private static final String PREF_NAME = "org.chromium.chrome.browser.upgrade.NotificationUpdateTimeStampPreferences";
     private static final String MILLISECONDS_NAME = "org.chromium.chrome.browser.upgrade.Milliseconds";
     private static final String URL = "https://brave.com/new-brave-22-percent-faster/";
-    private static final List<String> mWhitelistedRegionalLocales = Arrays.asList("en", "ru", "uk", "be", "pt", "fr");
+    private static final List<String> mWhitelistedRegionalLocales = Arrays.asList("en", "ru", "uk", "be", "pt", "fr", "es");
     private static final int NOTIFICATION_ID = 632;
     private static final String NOTIFICATION_DISMISS_EXTRA = "org.chromium.chrome.browser.upgrade.NotificationUpgrade";
 
