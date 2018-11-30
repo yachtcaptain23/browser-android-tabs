aa20918677c7f7be39bf887df305334975fa3837
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2018-11-30 16:38:14 -0500

    JNI link to RewardsService

--- chrome/android/java/src/org/chromium/chrome/browser/BraveRewardsPanelPopup.java
+++ chrome/android/java/src/org/chromium/chrome/browser/BraveRewardsPanelPopup.java
@@ -1,24 +1,47 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package org.chromium.chrome.browser;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
+import org.chromium.base.ApplicationStatus;
+import org.chromium.base.Log;
 import org.chromium.base.SysUtils;
+import org.chromium.chrome.browser.BraveRewardsHelper;
+import org.chromium.chrome.browser.BraveRewardsObserver;
+import org.chromium.chrome.browser.ChromeTabbedActivity;
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tabmodel.TabModel.TabLaunchType;
+import org.chromium.chrome.browser.tabmodel.TabModelSelectorImpl;
 import org.chromium.chrome.R;
+import org.chromium.content_public.browser.LoadUrlParams;
+
+import java.lang.ref.WeakReference;
 
 
-public class BraveRewardsPanelPopup {
+public class BraveRewardsPanelPopup implements BraveRewardsObserver {
     protected final View anchor;
     private final PopupWindow window;
+    private final BraveRewardsPanelPopup thisObject;
     private View root;
+    private Button btJoinRewards;
+    private TextView tvLearnMore;
+    private BraveRewardsNativeWorker mBraveRewardsNativeWorker;
 
     public BraveRewardsPanelPopup(View anchor) {
         this.anchor = anchor;
         this.window = new PopupWindow(anchor.getContext());
+        thisObject = this;
 
         this.window.setTouchInterceptor(new View.OnTouchListener() {
             @Override
@@ -30,6 +53,21 @@ public class BraveRewardsPanelPopup {
                 return false;
             }
         });
+        this.window.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                if (mBraveRewardsNativeWorker == null) {
+                  return;
+                }
+                mBraveRewardsNativeWorker.RemoveObserver(thisObject);
+            }
+        });
+        ChromeTabbedActivity activity = BraveRewardsHelper.GetChromeTabbedActivity();
+        mBraveRewardsNativeWorker = activity.getBraveRewardsNativeWorker();
+        if (mBraveRewardsNativeWorker != null) {
+          mBraveRewardsNativeWorker.Init();
+          mBraveRewardsNativeWorker.AddObserver(thisObject);
+        }
 
         onCreate();
     }
@@ -40,6 +78,29 @@ public class BraveRewardsPanelPopup {
 
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.brave_rewards_panel, null);
         setContentView(root);
+        btJoinRewards = (Button)root.findViewById(R.id.join_rewards_id);
+        if (btJoinRewards != null) {
+          btJoinRewards.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG", "!!!onClick bt");
+                if (mBraveRewardsNativeWorker != null) {
+                  mBraveRewardsNativeWorker.CreateWallet();
+                }
+            }
+          }));
+        }
+        tvLearnMore = (TextView)root.findViewById(R.id.learn_more_id);
+        if (tvLearnMore != null) {
+          tvLearnMore.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG", "!!!onClick tv");
+                launchTabInRunningTabbedActivity(new LoadUrlParams("chrome://rewards"));
+                dismiss();
+            }
+          }));
+        }
     }
 
     protected void onShow() {}
@@ -80,5 +141,22 @@ public class BraveRewardsPanelPopup {
 
     public void dismiss() {
         this.window.dismiss();
+    }
+
+    private Tab launchTabInRunningTabbedActivity(LoadUrlParams loadUrlParams) {
+        ChromeTabbedActivity activity = BraveRewardsHelper.GetChromeTabbedActivity();
+        if (activity == null || activity.getTabModelSelector() == null) return null;
+
+        TabModelSelectorImpl tabbedModeTabModelSelector = (TabModelSelectorImpl) activity.getTabModelSelector();
+        Tab tab = tabbedModeTabModelSelector.openNewTab(
+                loadUrlParams, TabLaunchType.FROM_BROWSER_ACTIONS, null, false);
+        assert tab != null;
+
+        return tab;
+    }
+
+    @Override
+    public void OnWalletCreated() {
+      Log.i("TAG", "!!!in observer OnWalletCreated");
     }
 }
