b8cda0192276a1a7b0d53763da90719b91b49137
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2019-01-04 12:02:17 -0500

    updated Site banner and Tip sent UI

--- chrome/android/java/src/org/chromium/chrome/browser/BraveRewardsDonationSentActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/BraveRewardsDonationSentActivity.java
@@ -1,22 +1,66 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package org.chromium.chrome.browser;
 
 import org.chromium.base.Log;
 import org.chromium.chrome.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 
-public class BraveRewardsDonationSentActivity extends Activity implements View.OnClickListener{
+public class BraveRewardsDonationSentActivity extends Activity {
+
+    private final int SLIDE_UP_DURATION = 3000;
+    private final int FADE_OUT_DURATION = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.brave_rewards_donation_sent);
-        findViewById(R.id.donation_sent_transparent_view).setOnClickListener(this);        
-    }
 
-   @Override
-   public void onClick(View v) {
-      finish();
-   }
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int height = metrics.heightPixels;
+        View floater = findViewById(R.id.floater);
+
+        //slide up animation
+        TranslateAnimation animate = new TranslateAnimation(0,0,height,0);
+        animate.setDuration(SLIDE_UP_DURATION);
+        animate.setFillAfter(true);
+        animate.setAnimationListener(new Animation.AnimationListener(){
+            @Override
+            public void onAnimationStart(Animation arg0) { }
+            @Override
+            public void onAnimationRepeat(Animation arg0) {}
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+
+                //fade out animation
+                Animation fadeOut = new AlphaAnimation(1, 0);
+                fadeOut.setInterpolator(new AccelerateInterpolator());
+                fadeOut.setStartOffset(0);
+                fadeOut.setDuration(FADE_OUT_DURATION);
+
+                fadeOut.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation arg0) {}
+                    @Override
+                    public void onAnimationRepeat(Animation arg0) {}
+                    @Override
+                    public void onAnimationEnd(Animation arg0) {
+                        finish();
+                    }
+                });
+
+                findViewById(R.id.floater).setAnimation(fadeOut);
+            }
+        });
+        floater.startAnimation(animate);
+    }
 }
