7fda93e8210c08ded93c9b2f5d784e171c6333db
samartnik <artem@brave.com>
2019-01-14 11:26:57 -0500

    Fix custom tab toolbar. Fixed panel for tablet mode. Removed PopupActivity.

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsDelegateAndroid.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsDelegateAndroid.java
@@ -28,7 +28,6 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.AppHooks;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.FullscreenActivity;
-import org.chromium.chrome.browser.PopupActivity;
 import org.chromium.chrome.browser.RepostFormWarningDialog;
 import org.chromium.chrome.browser.document.DocumentUtils;
 import org.chromium.chrome.browser.document.DocumentWebContentsDelegate;
@@ -305,11 +304,7 @@ public class TabWebContentsDelegateAndroid extends WebContentsDelegateAndroid {
     @Override
     public void openNewTab(String url, String extraHeaders, ResourceRequestBody postData,
             int disposition, boolean isRendererInitiated) {
-        if (!url.equals("chrome://rewards-donate/")) {
-            mTab.openNewTab(url, extraHeaders, postData, disposition, true, isRendererInitiated);
-        } else {
-            PopupActivity.show(mTab.getActivity(), url);
-        }
+        mTab.openNewTab(url, extraHeaders, postData, disposition, true, isRendererInitiated);
     }
 
     private Pair<WebContents, String> mWebContentsUrlMapping;
