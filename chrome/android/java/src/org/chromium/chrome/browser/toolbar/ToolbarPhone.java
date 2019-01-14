a1b6b6d2ae920fd27678a96259595fb0ce39f19d
samartnik <artem@brave.com>
2019-01-14 11:26:57 -0500

    Fix custom tab toolbar. Fixed panel for tablet mode. Removed PopupActivity.

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarPhone.java
@@ -669,11 +669,6 @@ public class ToolbarPhone
         }
         else if (mBraveRewardsPanelButton == v) {
             if (null != mBraveRewardsPanelButton) {
-                //mBraveShieldsListener.onClick(mBraveShieldsButton);
-                //RecordUserAction.record("MobileToolbarShowBraveShields");
-                //PopupActivity.show((ChromeActivity) ApplicationStatus.getLastTrackedFocusedActivity(), 
-                //    "chrome://rewards-panel");
-
                 BraveRewardsPanelPopup rewardsPopup = new BraveRewardsPanelPopup(v);
                 rewardsPopup.showLikePopDownMenu();
             }
