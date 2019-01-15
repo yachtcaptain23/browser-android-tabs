f21ad9e3a22d97b2e0a9811ee098096c3171d263
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2019-01-15 16:35:14 -0500

    Add funds handler

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -149,6 +149,7 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
     private ImageView mBraveRewardsPanelButton;
     private TextView mBraveRewardsNotificationsCount;
     private BraveRewardsNativeWorker mBraveRewardsNativeWorker;
+    private BraveRewardsPanelPopup mRewardsPopup;
 
     private ObjectAnimator mTabSwitcherModeAnimation;
     private ObjectAnimator mDelayedTabSwitcherModeAnimation;
@@ -578,11 +579,25 @@ public class ToolbarPhone extends ToolbarLayout implements Invalidator.Client, O
                 mBraveShieldsListener.onClick(mBraveShieldsButton);
             }
         } else if (mBraveRewardsPanelButton == v) {
-            if (null != mBraveRewardsPanelButton) {
-                BraveRewardsPanelPopup rewardsPopup = new BraveRewardsPanelPopup(v);
-                rewardsPopup.showLikePopDownMenu();
+            if (null == mRewardsPopup){
+              mRewardsPopup = new BraveRewardsPanelPopup(v);
+              mRewardsPopup.showLikePopDownMenu();
             }
         }
+    }
+
+    @Override
+    public  void onRewardsPanelDismiss() {
+        mRewardsPopup = null;
+    }
+
+
+    @Override
+    public  void dismissRewardsPanel() {
+      if ( null != mRewardsPopup ) {
+         mRewardsPopup.dismiss();
+         mRewardsPopup = null;
+      }
     }
 
     @Override
