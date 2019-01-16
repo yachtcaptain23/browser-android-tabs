be6fca7f5a2c1a3d33225b76a58d491437d00dfb
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2019-01-16 11:18:53 -0500

    corrected notifications time

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -2056,7 +2056,7 @@ public class ToolbarPhone
     public void OnGetCurrentBalanceReport(String[] report) {}
 
     @Override
-    public void OnNotificationAdded(String id, int type, int timestamp,
+    public void OnNotificationAdded(String id, int type, long timestamp,
             String[] args) {
         if (mBraveRewardsNativeWorker != null) {
             mBraveRewardsNativeWorker.GetAllNotifications();
@@ -2085,7 +2085,7 @@ public class ToolbarPhone
     }
 
     @Override
-    public void OnGetLatestNotification(String id, int type, int timestamp,
+    public void OnGetLatestNotification(String id, int type, long timestamp,
             String[] args) {}
 
     @Override
