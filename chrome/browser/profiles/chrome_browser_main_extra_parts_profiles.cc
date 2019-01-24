4d4249188a74e078d326591b56845c3b12c83048
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2019-01-24 16:04:17 -0500

    register rewards service

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -9,6 +9,7 @@
 
 #include "build/build_config.h"
 #include "brave/components/brave_ads/browser/ads_service_factory.h"
+#include "brave/components/brave_rewards/browser/rewards_service_factory.h"
 #include "chrome/browser/autocomplete/in_memory_url_index_factory.h"
 #include "chrome/browser/autocomplete/shortcuts_backend_factory.h"
 #include "chrome/browser/autofill/personal_data_manager_factory.h"
@@ -234,6 +235,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   BookmarkModelFactory::GetInstance();
   BookmarkUndoServiceFactory::GetInstance();
   brave_ads::AdsServiceFactory::GetInstance();
+  brave_rewards::RewardsServiceFactory::GetInstance();
   BrowsingDataHistoryObserverService::Factory::GetInstance();
   browser_sync::UserEventServiceFactory::GetInstance();
 #if BUILDFLAG(ENABLE_CAPTIVE_PORTAL_DETECTION)
