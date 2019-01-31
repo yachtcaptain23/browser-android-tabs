11e7054c265ba2f9feddb8ac864c6ebed34a47b0
Albert Wang <amwang217@gmail.com>
2019-01-30 19:24:16 -0800

    Update the URL bar which is the parent view of the LocationBar. Keep the LocationBar color consistent across all page and between incognito mode and not

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 package org.chromium.chrome.browser.toolbar;
+import android.util.Log;
 
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -227,7 +228,7 @@ public class ToolbarManager
     private boolean mInitializedWithNative;
     private Runnable mOnInitializedRunnable;
 
-    private boolean mShouldUpdateToolbarPrimaryColor = false;
+    private boolean mShouldUpdateToolbarPrimaryColor = true;
     private int mCurrentThemeColor;
 
     private OmniboxStartupMetrics mOmniboxStartupMetrics;
