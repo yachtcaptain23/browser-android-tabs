015b71636bf733eaa85e828503b7180c271068ea
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2019-02-07 16:55:35 -0500

    Remove panel and donation web-ui leftovers

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -26,8 +26,6 @@
 #include "chrome/browser/ui/webui/autofill_internals_ui.h"
 #include "chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals_ui.h"
 #include "chrome/browser/ui/webui/chromeos/account_manager_welcome_ui.h"
-#include "chrome/browser/ui/webui/brave_rewards_donate_ui.h"
-#include "chrome/browser/ui/webui/brave_rewards_panel_ui.h"
 #include "chrome/browser/ui/webui/components_ui.h"
 #include "chrome/browser/ui/webui/constrained_web_dialog_ui.h"
 #include "chrome/browser/ui/webui/crashes_ui.h"
