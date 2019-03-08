fed5a3e695f8cb2e9427210fc076c870f0ae45ec
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2019-03-08 10:12:04 -0500

    Use adblock and tp directly from brave-core DEPS

--- scripts/updateSubModules.js
+++ scripts/updateSubModules.js
@@ -1,21 +1,11 @@
-git submodule update --init hashset-cpp
-git submodule update --init tracking-protection
-git submodule update --init bloom-filter-cpp
-git submodule update --init ad-block
 git submodule update --init braveSync
 git submodule update --init brave-crypto
-git submodule update --init niceware
-git submodule update hashset-cpp
-git submodule update tracking-protection
-git submodule update bloom-filter-cpp
-git submodule update ad-block
 git submodule update braveSync
 git submodule update brave-crypto
-git submodule update niceware
 cd braveSync
-yarn install
-yarn run build
+npm install
+npm run build
 cd ..
 cd brave-crypto
-yarn install
-yarn run build
+npm install
+npm run build
