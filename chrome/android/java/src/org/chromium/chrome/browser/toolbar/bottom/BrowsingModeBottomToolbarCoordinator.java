bdba286f159b987f1257a9d06055a1b9e579a323
samartnik <artem@brave.com>
2019-03-11 12:12:56 -0400

    Fixed dynamic bottom toolbar feature

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/bottom/BrowsingModeBottomToolbarCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/bottom/BrowsingModeBottomToolbarCoordinator.java
@@ -22,6 +22,7 @@ import org.chromium.chrome.browser.fullscreen.ChromeFullscreenManager;
 import org.chromium.chrome.browser.modelutil.PropertyModelChangeProcessor;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+import org.chromium.chrome.browser.toolbar.BookmarksButton;
 import org.chromium.chrome.browser.toolbar.HomeButton;
 import org.chromium.chrome.browser.toolbar.MenuButton;
 import org.chromium.chrome.browser.toolbar.NewTabButton;
@@ -46,10 +47,10 @@ public class BrowsingModeBottomToolbarCoordinator {
     private final HomeButton mHomeButton;
 
     /** The bookmarks button that lives in the bottom toolbar. */
-    private final ImageButton mBookmarksButton;
+    private final BookmarksButton mBookmarksButton;
 
     /** The new tab button that lives in the bottom toolbar. */
-    private NewTabButton mNewTabButton;
+    private BottomToolbarNewTabButton mNewTabButton;
 
     /** The tab switcher button component that lives in the bottom toolbar. */
     private final TabSwitcherButtonCoordinator mTabSwitcherButtonCoordinator;
@@ -151,23 +152,23 @@ public class BrowsingModeBottomToolbarCoordinator {
         mMediator.setOverviewModeBehavior(overviewModeBehavior);
         mMediator.setThemeColorProvider(themeColorProvider);
 
-        // (Albert Wang): Don't use theme changes
-        // mHomeButton.setThemeColorProvider(themeColorProvider);
+        mHomeButton.setThemeColorProvider(themeColorProvider);
         //mShareButton.setThemeColorProvider(themeColorProvider);
         //mSearchAccelerator.setThemeColorProvider(themeColorProvider);
 
         mTabSwitcherButtonCoordinator.setTabSwitcherListener(tabSwitcherListener);
-        // (Albert Wang): Don't use theme changes
-        // mTabSwitcherButtonCoordinator.setThemeColorProvider(themeColorProvider);
+        mTabSwitcherButtonCoordinator.setThemeColorProvider(themeColorProvider);
         mTabSwitcherButtonCoordinator.setTabCountProvider(tabCountProvider);
 
         mNewTabButton = mToolbarRoot.findViewById(R.id.new_tab_button);
+        mNewTabButton.setThemeColorProvider(themeColorProvider);
         mNewTabButton.setOnClickListener(newTabClickListener);
 
         mMenuButton.setTouchListener(menuButtonHelper);
-        // (Albert Wang): Don't use theme changes
-        // mMenuButton.setThemeColorProvider(themeColorProvider);
+        mMenuButton.setThemeColorProvider(themeColorProvider);
         mMenuButton.setAccessibilityDelegate(menuButtonHelper);
+
+        mBookmarksButton.setThemeColorProvider(themeColorProvider);
     }
 
     /**
@@ -224,5 +225,7 @@ public class BrowsingModeBottomToolbarCoordinator {
         //mSearchAccelerator.destroy();
         mTabSwitcherButtonCoordinator.destroy();
         mMenuButton.destroy();
+        mNewTabButton.destroy();
+        mBookmarksButton.destroy();
     }
 }
