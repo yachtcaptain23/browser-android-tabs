d3304cc4efefefc934bca7ecf623cb4d091930c6
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2019-03-11 16:55:10 -0400

    Make mono apk buildable after Brave Rewards merge

--- brave_src/browser/brave_tab_helpers.cc
+++ brave_src/browser/brave_tab_helpers.cc
@@ -5,12 +5,14 @@
 #include "brave_src/browser/brave_tab_helpers.h"
 
 #include "brave_src/browser/brave_tab_url_web_contents_observer.h"
+#include "brave_src/browser/web_contents_ledger_observer.h"
 #include "content/public/browser/web_contents.h"
 
 namespace brave {
 
 void AttachTabHelpers(content::WebContents* web_contents) {
   brave::BraveTabUrlWebContentsObserver::CreateForWebContents(web_contents);
+  brave::WebContentsLedgerObserver::CreateForWebContents(web_contents);
 }
 
 }  // namespace brave
