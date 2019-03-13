b72b068caacd051ec1ecb79840babf7728c4a4a7
Albert Wang <amwang217@gmail.com>
2019-03-13 00:58:28 -0700

    Enable SearchAccelerator at the bottom toolbar

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/SearchAccelerator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/SearchAccelerator.java
@@ -13,15 +13,16 @@ import android.util.AttributeSet;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.toolbar.ThemeColorProvider;
-import org.chromium.chrome.browser.toolbar.ThemeColorProvider.ThemeColorObserver;
+import org.chromium.chrome.browser.ThemeColorProvider;
+import org.chromium.chrome.browser.ThemeColorProvider.ThemeColorObserver;
+import org.chromium.chrome.browser.ThemeColorProvider.TintObserver;
 import org.chromium.chrome.browser.util.ColorUtils;
 import org.chromium.ui.widget.ChromeImageButton;
 
 /**
  * The search accelerator.
  */
-class SearchAccelerator extends ChromeImageButton implements ThemeColorObserver {
+class SearchAccelerator extends ChromeImageButton implements ThemeColorObserver, TintObserver {
     /** A provider that notifies components when the theme color changes.*/
     private ThemeColorProvider mThemeColorProvider;
 
@@ -30,24 +31,25 @@ class SearchAccelerator extends ChromeImageButton implements ThemeColorObserver 
 
     public SearchAccelerator(Context context, AttributeSet attrs) {
         super(context, attrs);
-
         mResources = context.getResources();
     }
 
     void setThemeColorProvider(ThemeColorProvider themeColorProvider) {
         mThemeColorProvider = themeColorProvider;
-        mThemeColorProvider.addObserver(this);
+        mThemeColorProvider.addThemeColorObserver(this);
+        mThemeColorProvider.addTintObserver(this);
     }
 
     void destroy() {
         if (mThemeColorProvider != null) {
-            mThemeColorProvider.removeObserver(this);
+            mThemeColorProvider.removeThemeColorObserver(this);
+            mThemeColorProvider.removeTintObserver(this);
             mThemeColorProvider = null;
         }
     }
 
     @Override
-    public void onThemeColorChanged(ColorStateList tint, int primaryColor) {
+    public void onTintChanged(ColorStateList tint, boolean useLight) {
         ApiCompatibilityUtils.setImageTintList(this, tint);
     }
 }
