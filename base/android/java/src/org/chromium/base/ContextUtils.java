cbbd7f77b684d434f7d01c91bb82273a4bf19532
samartnik <artem@brave.com>
2019-05-31 10:42:47 -0400

    Fixed base application context

--- base/android/java/src/org/chromium/base/ContextUtils.java
+++ base/android/java/src/org/chromium/base/ContextUtils.java
@@ -51,7 +51,7 @@ public class ContextUtils {
 
     public static Context getBaseApplicationContext() {
         if (sApplicationContext instanceof ContextWrapper) {
-            return ((ContextWrapper)sApplicationContext).getBaseContext();
+            return ((ContextWrapper)sApplicationContext).getBaseContext().getApplicationContext();
         } else {
             return getApplicationContext();
         }
