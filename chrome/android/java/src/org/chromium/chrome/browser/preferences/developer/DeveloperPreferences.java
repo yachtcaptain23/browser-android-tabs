d2e81b95c4e3d270e7564bf81725640d3c172d21
samartnik <artem@brave.com>
2019-06-05 16:50:46 -0400

    Added additional info for QA

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/developer/DeveloperPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/developer/DeveloperPreferences.java
@@ -4,42 +4,22 @@
 
 package org.chromium.chrome.browser.preferences.developer;
 
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.support.v7.app.AlertDialog;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.EditText;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeVersionInfo;
-import org.chromium.chrome.browser.BraveRewardsNativeWorker;
-import org.chromium.chrome.browser.BraveRewardsObserver;
-import org.chromium.chrome.browser.BraveRewardsPanelPopup;
-import org.chromium.chrome.browser.ConfigAPIs;
-import org.chromium.chrome.browser.preferences.ChromeSwitchPreference;
 import org.chromium.chrome.browser.preferences.PreferenceUtils;
-import org.chromium.chrome.browser.preferences.PrefServiceBridge;
-import org.chromium.chrome.browser.RestartWorker;
 import org.chromium.components.version_info.Channel;
 import org.chromium.components.version_info.VersionConstants;
 
-import org.chromium.base.Log;
 /**
  * Settings fragment containing preferences aimed at Chrome and web developers.
  */
-public class DeveloperPreferences extends PreferenceFragment
-        implements OnPreferenceChangeListener, BraveRewardsObserver {
+public class DeveloperPreferences extends PreferenceFragment {
     private static final String UI_PREF_BETA_STABLE_HINT = "beta_stable_hint";
     private static final String PREF_DEVELOPER_ENABLED = "developer";
-    private static final String PREF_USE_REWARDS_STAGING_SERVER = "use_rewards_staging_server";
 
     // Non-translated strings:
     private static final String MSG_DEVELOPER_OPTIONS_TITLE = "Developer options";
@@ -47,7 +27,7 @@ public class DeveloperPreferences extends PreferenceFragment
     public static boolean shouldShowDeveloperPreferences() {
         // Always enabled on canary, dev and local builds, otherwise can be enabled by tapping the
         // Chrome version in Settings>About multiple times.
-        // if (VersionConstants.CHANNEL <= Channel.DEV) return true;
+        if (VersionConstants.CHANNEL <= Channel.DEV) return true;
         return ContextUtils.getAppSharedPreferences().getBoolean(PREF_DEVELOPER_ENABLED, false);
     }
 
@@ -67,129 +47,5 @@ public class DeveloperPreferences extends PreferenceFragment
         if (ChromeVersionInfo.isBetaBuild() || ChromeVersionInfo.isStableBuild()) {
             getPreferenceScreen().removePreference(findPreference(UI_PREF_BETA_STABLE_HINT));
         }
-
-        ChromeSwitchPreference stagingServer =
-                (ChromeSwitchPreference) findPreference(PREF_USE_REWARDS_STAGING_SERVER);
-        stagingServer.setOnPreferenceChangeListener(this);
     }
-
-    @Override
-    public void onStart() {
-        BraveRewardsNativeWorker.getInstance().AddObserver(this);
-        super.onStart();
-    }
-
-    @Override
-    public void onStop() {
-        BraveRewardsNativeWorker.getInstance().RemoveObserver(this);
-        super.onStop();
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        checkQACode(preference, newValue);
-        return true;
-    }
-
-    private void checkQACode(Preference preference, Object newValue) {
-        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(
-                Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.qa_code_check, null);
-        final EditText input = (EditText) view.findViewById(R.id.qa_code);
-
-        DialogInterface.OnClickListener onClickListener = new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int button) {
-                if (button == AlertDialog.BUTTON_POSITIVE &&
-                    input.getText().toString().equals(ConfigAPIs.QA_CODE)) {
-                    if (PREF_USE_REWARDS_STAGING_SERVER.equals(preference.getKey())) {
-                        PrefServiceBridge.getInstance().setUseRewardsStagingServer((boolean) newValue);
-                        BraveRewardsNativeWorker.getInstance().ResetTheWholeState();
-                        RestartWorker.AskForRelaunch(getActivity());
-                    }
-                } else {
-                    if (preference instanceof ChromeSwitchPreference) {
-                        ((ChromeSwitchPreference)preference).setChecked(!(boolean)newValue);
-                    }
-                }
-            }
-        };
-
-        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity(), R.style.Theme_Chromium_AlertDialog);
-        if (null == alert) {
-            return;
-        }
-        AlertDialog.Builder alertDialog = alert
-                .setTitle("Enter QA code")
-                .setView(view)
-                .setPositiveButton(R.string.ok, onClickListener)
-                .setNegativeButton(R.string.cancel, onClickListener)
-                .setCancelable(false);
-        Dialog dialog = alertDialog.create();
-        dialog.setCanceledOnTouchOutside(false);
-        dialog.show();
-    }
-
-    @Override
-    public void OnWalletInitialized(int error_code) {}
-
-    @Override
-    public void OnWalletProperties(int error_code) {}
-
-    @Override
-    public void OnPublisherInfo(int tabId) {}
-
-    @Override
-    public void OnGetCurrentBalanceReport(String[] report) {}
-
-    @Override
-    public void OnNotificationAdded(String id, int type, long timestamp,
-          String[] args) {}
-
-    @Override
-    public void OnNotificationsCount(int count) {}
-
-    @Override
-    public void OnGetLatestNotification(String id, int type, long timestamp,
-              String[] args) {}
-
-    @Override
-    public void OnNotificationDeleted(String id) {}
-
-    @Override
-    public void OnIsWalletCreated(boolean created) {}
-
-    @Override
-    public void OnGetPendingContributionsTotal(double amount) {}
-
-    @Override
-    public void OnGetRewardsMainEnabled(boolean enabled) {
-    }
-
-    @Override
-    public void OnGetAutoContributeProps() {}
-
-    @Override
-    public void OnGetReconcileStamp(long timestamp) {}
-
-    @Override
-    public void OnRecurringDonationUpdated() {}
-
-    @Override
-    public void OnResetTheWholeState(boolean success) {
-        if (success) {
-            SharedPreferences sharedPreferences = ContextUtils.getAppSharedPreferences();
-            SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
-            sharedPreferencesEditor.putBoolean(BraveRewardsPanelPopup.PREF_GRANTS_NOTIFICATION_RECEIVED, false);
-            sharedPreferencesEditor.putBoolean(BraveRewardsPanelPopup.PREF_WAS_BRAVE_REWARDS_TURNED_ON, false);
-            sharedPreferencesEditor.apply();
-            PrefServiceBridge.getInstance().setSafetynetCheckFailed(false);
-            RestartWorker.AskForRelaunch(getActivity());
-        } else {
-            RestartWorker.AskForRelaunchCustom(getActivity());
-        }
-    }
-
-    @Override
-    public void OnRewardsMainEnabled(boolean enabled) {}
 }
